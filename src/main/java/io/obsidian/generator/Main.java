374ec0330fd755e09380cd136822b102ecc4040b
George Gastaldi <gegastaldi@gmail.com>
2016-12-07 13:10:58 -0200

    Moving keystore to src/main/resources

--- src/main/java/io/obsidian/generator/Main.java
+++ src/main/java/io/obsidian/generator/Main.java
@@ -7,6 +7,12 @@
 
 package io.obsidian.generator;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.undertow.UndertowFraction;
 
@@ -19,7 +25,26 @@ public class Main
    public static void main(String[] args) throws Exception
    {
       Swarm swarm = new Swarm();
-      swarm.fraction(UndertowFraction.createDefaultFraction("keystore.jks", "password", "appserver"));
+      Path keyStorePath = keystorePath();
+      swarm.fraction(UndertowFraction.createDefaultFraction(keyStorePath.toString(), "password", "appserver"));
       swarm.start().deploy();
+   }
+
+   private static Path keystorePath() throws IOException
+   {
+      // Moving keystore to tmp file
+      Path tmpFile = Files.createTempFile("keystore", ".jks");
+      try (InputStream is = Main.class.getClassLoader().getResourceAsStream("keystore.jks"))
+      {
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         byte[] b = new byte[4096];
+         int i = -1;
+         while ((i = is.read(b)) != -1)
+         {
+            baos.write(b, 0, i);
+         }
+         Files.write(tmpFile, baos.toByteArray());
+      }
+      return tmpFile;
    }
 }
