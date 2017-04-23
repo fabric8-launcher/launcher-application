c3f0401f2f0571da2129445e24aadc3412ac5f6b
George Gastaldi <gegastaldi@gmail.com>
2017-04-23 17:09:40 -0300

    Update HotDeployListener.java

--- src/main/java/io/openshift/launchpad/backend/HotDeployListener.java
+++ src/main/java/io/openshift/launchpad/backend/HotDeployListener.java
@@ -18,7 +18,8 @@ import org.jboss.forge.addon.manager.watch.AddonWatchService;
 import io.openshift.launchpad.backend.event.FurnaceStartup;
 
 /**
- * Enables the hot deployment feature in Forge
+ * Enables the hot deployment feature in Forge.
+ * Make sure to build this project with -DdevMode=true too
  *
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
