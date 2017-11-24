ae435df7393b0acb63b1bb115c88a15b20ef36d0
George Gastaldi <gegastaldi@gmail.com>
2017-11-24 15:20:08 +0000

    Added Results + tests

---
+++ web/src/main/java/io/fabric8/launcher/web/forge/util/Results.java
@@ -0,0 +1,75 @@
+package io.fabric8.launcher.web.forge.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.jboss.forge.addon.ui.result.CompositeResult;
+import org.jboss.forge.addon.ui.result.Result;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class Results {
+
+    /**
+     * @param result
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static Map<String, String> getEntityAsMap(Result result) {
+        if (result instanceof CompositeResult) {
+            for (Result singleResult : ((CompositeResult) result).getResults()) {
+                Object obj = singleResult.getEntity().orElse(null);
+                if (obj instanceof Map) {
+                    return (Map<String, String>) obj;
+                }
+            }
+        }
+        return Collections.emptyMap();
+    }
+
+
+    public static Object getEntity(Result result) {
+        if (result == null) {
+            return null;
+        } else if (result instanceof CompositeResult) {
+            CompositeResult compositeResult = (CompositeResult) result;
+            List<Object> answer = new ArrayList<>();
+            List<Result> results = compositeResult.getResults();
+            for (Result child : results) {
+                Object entity = getEntity(child);
+                answer.add(entity);
+            }
+            return answer;
+        }
+        return result.getEntity().orElse(null);
+    }
+
+    /**
+     * Returns the result message handling composite results
+     */
+    public static String getMessage(Result result) {
+        if (result == null) {
+            return null;
+        } else if (result instanceof CompositeResult) {
+            CompositeResult compositeResult = (CompositeResult) result;
+            StringBuilder builder = new StringBuilder();
+            List<Result> results = compositeResult.getResults();
+            for (Result child : results) {
+                String message = getMessage(child);
+                if (message != null && message.trim().length() > 0) {
+                    if (builder.length() > 0) {
+                        builder.append("\n");
+                    }
+                    builder.append(message);
+                }
+            }
+            return builder.toString();
+
+        }
+        return result.getMessage();
+    }
+
+}
---
+++ web/src/test/java/io/fabric8/launcher/web/forge/util/ResultsTest.java
@@ -0,0 +1,82 @@
+package io.fabric8.launcher.web.forge.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class ResultsTest {
+
+    @Test
+    public void findReturnMapWithCompositeResultShouldBeSame() {
+        Map<String, String> map = Collections.singletonMap("key", "value");
+        Result result = Results.aggregate(Arrays.asList(Results.success("Success", map)));
+        Map<String, String> returnMap = io.fabric8.launcher.web.forge.util.Results.getEntityAsMap(result);
+        assertThat(returnMap).isSameAs(map);
+    }
+
+    @Test
+    public void findReturnMapWithSingleResultShouldBeSame() {
+        Map<String, String> map = Collections.singletonMap("key", "value");
+        Result result = Results.success("Success", map);
+        Map<String, String> returnMap = io.fabric8.launcher.web.forge.util.Results.getEntityAsMap(result);
+        assertThat(returnMap).isEmpty();
+    }
+
+    @Test
+    public void findReturnMapWithNullResultShouldBeSame() {
+        Map<String, String> returnMap = io.fabric8.launcher.web.forge.util.Results.getEntityAsMap(null);
+        assertThat(returnMap).isEmpty();
+    }
+
+    @Test
+    public void getEntityWithSingleResultReturnsEntity() {
+        Map<String, String> map = Collections.singletonMap("key", "value");
+        Result result = Results.success("Success", map);
+        Object returnMap = io.fabric8.launcher.web.forge.util.Results.getEntity(result);
+        assertThat(returnMap).isSameAs(map);
+    }
+
+    @Test
+    public void getEntityWithCompositeResultReturnsList() {
+        Result result = Results.aggregate(Arrays.asList(Results.success(), Results.fail()));
+        Object ret = io.fabric8.launcher.web.forge.util.Results.getEntity(result);
+        assertThat(ret).isInstanceOf(List.class);
+        assertThat((List<String>) ret).hasSize(2);
+    }
+
+    @Test
+    public void getEntityWithNullResultReturnsNull() {
+        Object ret = io.fabric8.launcher.web.forge.util.Results.getEntity(null);
+        assertThat(ret).isNull();
+    }
+
+    @Test
+    public void getMessageWithSingleResultReturnsSingleMessage() {
+        Result result = Results.success("Everything is fine");
+        String message = io.fabric8.launcher.web.forge.util.Results.getMessage(result);
+        assertThat(message).isEqualTo(result.getMessage());
+    }
+
+    @Test
+    public void getMessageWithCompositeResultReturnsConcatenatedMessage() {
+        Result result = Results.aggregate(Arrays.asList(Results.success("A"), Results.fail("B")));
+        String message = io.fabric8.launcher.web.forge.util.Results.getMessage(result);
+        assertThat(message).isEqualTo("A\nB");
+    }
+
+    @Test
+    public void getMessageWithNullResultReturnsNull() {
+        Object ret = io.fabric8.launcher.web.forge.util.Results.getMessage(null);
+        assertThat(ret).isNull();
+    }
+}
