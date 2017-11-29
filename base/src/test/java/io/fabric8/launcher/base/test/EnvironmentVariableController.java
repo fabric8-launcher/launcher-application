1c22eb0dc6a9327031610bcf8ba05b38e769fe34
George Gastaldi <gegastaldi@gmail.com>
2017-11-29 16:45:42 -0200

    EnvironmentVariableController is no longer broken

--- base/src/test/java/io/fabric8/launcher/base/test/EnvironmentVariableController.java
+++ base/src/test/java/io/fabric8/launcher/base/test/EnvironmentVariableController.java
@@ -1,8 +1,7 @@
 package io.fabric8.launcher.base.test;
 
 import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.HashMap;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -20,39 +19,28 @@ public class EnvironmentVariableController {
      * http://stackoverflow.com/questions/318239/how-do-i-set-environment-variables-from-java
      */
     public static void setEnv(final String name, final String value) {
-        Map<String, String> newenv = new HashMap<String, String>();
-        newenv.put(name, value);
         try {
             Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
             Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
             theEnvironmentField.setAccessible(true);
+            Class<?> variableClass = Class.forName("java.lang.ProcessEnvironment$Variable");
+            Class<?> valueClass = Class.forName("java.lang.ProcessEnvironment$Value");
 
-            Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
-            env.putAll(newenv);
-            Field theCaseInsensitiveEnvironmentField = processEnvironmentClass
-                    .getDeclaredField("theCaseInsensitiveEnvironment");
-            theCaseInsensitiveEnvironmentField.setAccessible(true);
-            Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
-            cienv.putAll(newenv);
-        } catch (NoSuchFieldException e) {
-            try {
-                Class[] classes = Collections.class.getDeclaredClasses();
-                Map<String, String> env = System.getenv();
-                for (Class cl : classes) {
-                    if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
-                        Field field = cl.getDeclaredField("m");
-                        field.setAccessible(true);
-                        Object obj = field.get(env);
-                        Map<String, String> map = (Map<String, String>) obj;
-                        map.clear();
-                        map.putAll(newenv);
-                    }
-                }
-            } catch (Exception e2) {
-                //NOOP
+            Map<Object, Object> envs = (Map<Object, Object>) theEnvironmentField.get(null);
+            Method variableValueOfMethod = variableClass.getMethod("valueOf", String.class);
+            variableValueOfMethod.setAccessible(true);
+            Object variable = variableValueOfMethod.invoke(null, name);
+            if (value == null) {
+                envs.remove(variable);
+            } else {
+                Method valueValueOfMethod = valueClass.getMethod("valueOf", String.class);
+                valueValueOfMethod.setAccessible(true);
+                Object valueObj = valueValueOfMethod.invoke(null, value);
+                envs.put(variable, valueObj);
             }
         } catch (Exception e1) {
             //NOOP
+            e1.printStackTrace();
         }
     }
 
