ce390c5e2cff52f0082bdc7dd06f8bbfa185ed32
George Gastaldi <gegastaldi@gmail.com>
2017-12-15 11:17:50 -0200

    Using depth=1 to fetch only latest version

--- addons/launcher-addon/add-template-files.sh
+++ addons/launcher-addon/add-template-files.sh
@@ -7,6 +7,6 @@ git init
 git remote add origin https://github.com/fabric8-launcher/launcher-documentation.git
 git config core.sparseCheckout true
 echo "docs/topics/readme" >> .git/info/sparse-checkout
-git pull origin master
+git pull origin master --depth=1
 echo Copying readme files to $1
 mv docs/topics $1
