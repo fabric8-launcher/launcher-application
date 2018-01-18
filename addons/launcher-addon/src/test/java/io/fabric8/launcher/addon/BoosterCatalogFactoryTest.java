2ebe107875faa88153ec21f319c9c728a0759bd6
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-01-18 11:37:44 +0100

    feat: introduces in-memory git rules for BoosterCatalogFactoryTest (#19)
    * feat: introduces local git server for BoosterCatalogFactoryTest
    
    * fix(test): brings original test for default catalog (and soft assertions)
    
    * fix: makes git server running on any free port
    
    * fix: adds missing comment explaining what null stands for

--- addons/launcher-addon/src/test/java/io/fabric8/launcher/addon/BoosterCatalogFactoryTest.java
+++ addons/launcher-addon/src/test/java/io/fabric8/launcher/addon/BoosterCatalogFactoryTest.java
@@ -9,10 +9,13 @@ package io.fabric8.launcher.addon;
 
 import io.openshift.booster.catalog.BoosterCatalog;
 import io.openshift.booster.catalog.LauncherConfiguration;
+import org.arquillian.smart.testing.rules.git.server.GitServer;
+import org.assertj.core.api.JUnitSoftAssertions;
 import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.contrib.java.lang.system.ProvideSystemProperty;
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
@@ -21,9 +24,21 @@ public class BoosterCatalogFactoryTest {
 
     private BoosterCatalogFactory factory;
 
+    @ClassRule
+    public static GitServer gitServer = GitServer.fromBundle("booster-catalog", "repos/boosters/booster-catalog.bundle")
+       .usingAnyFreePort()
+       .create();
+
+    @Rule
+    public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
+
+    @Rule
+    public final ProvideSystemProperty boosterCatalogProperties =
+       new ProvideSystemProperty(LauncherConfiguration.PropertyName.LAUNCHER_BOOSTER_CATALOG_REF, "openshift-online-free")
+          .and(LauncherConfiguration.PropertyName.LAUNCHER_BOOSTER_CATALOG_REPOSITORY, "http://localhost:" + gitServer.getPort() + "/booster-catalog");
+
     @Before
     public void setUp() {
-        System.setProperty(LauncherConfiguration.PropertyName.LAUNCHER_BOOSTER_CATALOG_REF, "openshift-online-free");
         factory = new BoosterCatalogFactory();
         // Forcing CDI initialization here
         factory.reset();
@@ -32,16 +47,16 @@ public class BoosterCatalogFactoryTest {
     @Test
     public void testDefaultCatalogServiceNotNullAndIsSingleton() {
         BoosterCatalog defaultService = factory.getDefaultCatalog();
-        assertThat(defaultService).isNotNull();
-        assertThat(factory.getDefaultCatalog()).isSameAs(defaultService);
+        softly.assertThat(defaultService).isNotNull();
+        softly.assertThat(factory.getDefaultCatalog()).isSameAs(defaultService);
     }
 
     @Test
     public void testMasterCatalogIsNotSameAsDefault() {
         // A null catalogURL means use default repository URL
         BoosterCatalog masterService = factory.getCatalog(null, "master");
-        assertThat(masterService).isNotNull();
-        assertThat(factory.getDefaultCatalog()).isNotSameAs(masterService);
+        softly.assertThat(masterService).isNotNull();
+        softly.assertThat(factory.getDefaultCatalog()).isNotSameAs(masterService);
     }
 
 }
