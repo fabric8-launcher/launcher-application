b431018c4b5d7f7ecab5043c0e897a89a6430de3
George Gastaldi <gegastaldi@gmail.com>
2018-01-26 22:04:20 +0000

    Fixes resource signature

--- web/src/main/java/io/fabric8/launcher/web/api/UpdateGitHubWebHooksResource.java
+++ web/src/main/java/io/fabric8/launcher/web/api/UpdateGitHubWebHooksResource.java
@@ -1,9 +1,9 @@
 package io.fabric8.launcher.web.api;
 
+import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -15,6 +15,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.fabric8.forge.generator.keycloak.KeycloakClient;
 import io.fabric8.forge.generator.keycloak.KeycloakEndpoint;
@@ -34,10 +35,6 @@ import io.fabric8.launcher.service.github.api.GitHubServiceFactory;
 @ApplicationScoped
 public class UpdateGitHubWebHooksResource {
 
-
-    private static final Pattern pattern = Pattern.compile(
-            "https\\:\\/\\/jenkins.*\\.openshiftapps\\.com\\/github-webhook\\/?");
-
     @Inject
     GitHubServiceFactory gitHubServiceFactory;
 
@@ -45,21 +42,22 @@ public class UpdateGitHubWebHooksResource {
     @POST
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public String updateWebHook(@HeaderParam(HttpHeaders.AUTHORIZATION) String authHeader,
-                                @QueryParam("repository") String repository,
-                                @QueryParam("url") String newUrl) throws Exception {
+    public Response updateWebHook(@HeaderParam(HttpHeaders.AUTHORIZATION) String authHeader,
+                                  @QueryParam("repository") String repository,
+                                  @QueryParam("oldUrl") String oldUrl,
+                                  @QueryParam("newUrl") String newUrl) throws MalformedURLException {
         String gitHubToken = new KeycloakClient().getTokenFor(KeycloakEndpoint.GET_GITHUB_TOKEN, authHeader);
         GitHubService gitHub = gitHubServiceFactory.create(IdentityFactory.createFromToken(gitHubToken));
         GitRepository gitRepository = ImmutableGitRepository.builder().fullName(repository).homepage(URI.create("http://foo")).gitCloneUri(URI.create("http://foo")).build();
         List<GitHook> hooks = gitHub.getHooks(gitRepository);
         for (GitHook hook : hooks) {
-            if (pattern.matcher(hook.getUrl()).matches()) {
+            if (Objects.equals(oldUrl, hook.getUrl())) {
                 String[] events = hook.getEvents().toArray(new String[0]);
-                GitHook newHook = gitHub.createHook(gitRepository, new URL(newUrl), events);
+                GitHook newHook = gitHub.createHook(gitRepository, URI.create(newUrl).toURL(), events);
                 gitHub.deleteWebhook(gitRepository, hook);
-                return "Created " + newHook;
+                return Response.created(URI.create(newHook.getUrl())).build();
             }
         }
-        return "No webhooks found";
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
