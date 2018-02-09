3f76006801e9f18855808e6613a728b045e94962
George Gastaldi <gegastaldi@gmail.com>
2018-02-09 17:59:59 +0000

    fix(test): Run integration tests during build.
    Fixes https://github.com/fabric8-launcher/launcher-planning/issues/98
    
    Signed-off-by: George Gastaldi <gegastaldi@gmail.com>

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-. inherit-env
+# Exit on error
+set -e
+
+KEYCLOAK=OFFICIAL
+
+source launcher-env-template.sh
 
 yum -y update
 yum -y install centos-release-scl java-1.8.0-openjdk-devel git
 yum -y install rh-maven33
 
-scl enable rh-maven33 'mvn clean verify -B'
+scl enable rh-maven33 'mvn install failsafe:integration-test failsafe:verify -Pit -B'
 
 if [ $? -ne 0 ]; then
     echo 'Build Failed!'
