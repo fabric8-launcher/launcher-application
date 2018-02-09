c3dbe91c45da39e9b711928688126900db426165
George Gastaldi <gegastaldi@gmail.com>
2018-02-09 17:41:36 +0000

    chore(ci): Add git to the test script

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -3,7 +3,7 @@
 . inherit-env
 
 yum -y update
-yum -y install centos-release-scl java-1.8.0-openjdk-devel
+yum -y install centos-release-scl java-1.8.0-openjdk-devel git
 yum -y install rh-maven33
 
 scl enable rh-maven33 'mvn clean verify -B'
