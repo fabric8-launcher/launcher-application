bdb184b10d75bfffe04a8e9e08b2f168311d5ca7
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-02-16 17:20:44 +0100

    jenkins config xml parser

---
+++ addons/new-osio-addon/src/test/java/io/fabric8/launcher/osio/jenkins/JenkinsConfigParserTest.java
@@ -0,0 +1,117 @@
+package io.fabric8.launcher.osio.jenkins;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JenkinsConfigParserTest {
+
+    private static final String REPOSITORY_NAME = "testRepo";
+
+    @Test
+    public void setGithubOwner() throws IOException {
+        //given
+        String gitOwner = "testOwnerName";
+        String xml = parseTestXml("example-jenkins-config.xml");
+        JenkinsConfigParser parser = new JenkinsConfigParser(xml);
+
+        //when
+        parser.setGithubOwner(gitOwner);
+        String resultXml = parser.toXml();
+
+        //then
+        assertTrue("resultXml should be updated", resultXml.contains(gitOwner));
+    }
+
+    @Test
+    public void setRepository() throws IOException {
+        //when
+        String resultXml = transformXml("example-jenkins-config.xml");
+
+        //then
+        assertTrue("resultXml should be updated", resultXml.contains(REPOSITORY_NAME));
+    }
+
+    @Test
+    public void setRepositoryMissingTraits() throws IOException {
+        //when
+        String resultXml = transformXml("missing-traits-jenkins-config.xml");
+
+        //then
+        assertTrue("resultXml should be updated", resultXml.contains("oldName|" + REPOSITORY_NAME));
+    }
+
+    @Test
+    public void setRepositoryNoOldRepoName() throws IOException {
+        //when
+        String resultXml = transformXml("no-oldname-jenkins-config.xml");
+
+        //then
+        assertTrue("resultXml should be updated", resultXml.contains(">" + REPOSITORY_NAME));
+    }
+
+    @Test
+    public void setEmptyXml() throws IOException {
+        //given
+        String gitOwner = "testOwnerName";
+        JenkinsConfigParser parser = new JenkinsConfigParser("");
+
+        //when
+        parser.setGithubOwner(gitOwner);
+        String resultXml = parser.toXml();
+
+        //then
+        assertTrue("resultXml should be updated", resultXml.contains(gitOwner));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIllegalXml() {
+        //given
+        JenkinsConfigParser parser = new JenkinsConfigParser("notXml");
+
+        //then
+        parser.setRepository("dummy");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIllegalMissingTags() {
+        //given
+        JenkinsConfigParser parser = new JenkinsConfigParser("<app><org.jenkinsci.plugins.github__branch__source.GitHubSCMNavigator/></app>");
+
+        //then
+        parser.setRepository("dummy");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIllegalMissingTag() {
+        //given
+        JenkinsConfigParser parser = new JenkinsConfigParser("<app/>");
+
+        //then
+        parser.setRepository("dummy");
+    }
+
+    private String transformXml(String templateName) throws IOException {
+        String xml = parseTestXml(templateName);
+        JenkinsConfigParser parser = new JenkinsConfigParser(xml);
+
+        parser.setRepository(REPOSITORY_NAME);
+        return parser.toXml();
+    }
+
+    private String parseTestXml(String exampleXmlFilename) throws IOException {
+        InputStream resource = getClass().getResourceAsStream("/" + exampleXmlFilename);
+        return IOUtils.toString(resource, Charset.defaultCharset());
+    }
+}
