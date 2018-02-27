1f7ebc69d351c22bec8a8da6e75139f41482b6b5
George Gastaldi <gegastaldi@gmail.com>
2018-02-27 22:21:40 +0000

    feat(http): Introduced HttpExceptionMapper

---
+++ addons/new-osio-addon/src/main/java/io/fabric8/launcher/osio/http/HttpExceptionMapper.java
@@ -0,0 +1,16 @@
+package io.fabric8.launcher.osio.http;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@Provider
+public class HttpExceptionMapper implements ExceptionMapper<HttpException> {
+    @Override
+    public Response toResponse(HttpException exception) {
+        return Response.status(exception.getStatusCode()).build();
+    }
+}
