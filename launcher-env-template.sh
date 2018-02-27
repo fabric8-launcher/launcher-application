f93984f95733d60649b08e014c3310d612cac629
Andy Damevin <andy.damevin@neotys.com>
2018-02-27 15:45:07 +0100

    docs(README): Improve KeyCloak info in README.md
    Fixes #106

---
+++ launcher-env-template.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+#################################################
+# Here you can find basic setup for this script #
+#################################################
+
+
+# Before to source this script, set the KEYCLOAK variable to one of those options: NO, OFFICIAL, LOCAL.
+
+if [ -z "$KEYCLOAK" ]; then
+
+    # Default KeyCloak will be used if KEYCLOAK variable is not set before.
+    # Choose (uncomment) one of the 3 default KeyCloak options below.
+
+    KEYCLOAK=NO
+    #KEYCLOAK=OFFICIAL
+    #KEYCLOAK=LOCAL
+
+fi
+
+
+#################################################
+#################################################
+
+echo ----- Environment is using $KEYCLOAK KeyCloak -----
+
+SCRIPT_DIR=$(cd "$(dirname "$BASH_SOURCE")" ; pwd -P)
+
+#### DO NOT CHANGE - Reset environment variables
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_API_URL
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_USERNAME
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_PASSWORD
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_TOKEN
+unset LAUNCHER_MISSIONCONTROL_GITHUB_USERNAME
+unset LAUNCHER_MISSIONCONTROL_GITLAB_PRIVATE_TOKEN
+unset LAUNCHER_MISSIONCONTROL_GITLAB_USERNAME
+unset LAUNCHER_MISSIONCONTROL_GITHUB_TOKEN
+unset LAUNCHER_MISSIONCONTROL_BITBUCKET_USERNAME
+unset LAUNCHER_MISSIONCONTROL_BITBUCKET_APPLICATION_PASSWORD
+unset LAUNCHER_KEYCLOAK_URL
+unset LAUNCHER_KEYCLOAK_REALM
+unset LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE
+####
+
+case "$KEYCLOAK" in
+"NO")
+    # No KeyCloak
+    MSHIFT=$(minishift console --url)
+    if [[ $MSHIFT == *"://:"* ]]; then
+        echo "WARNING: MiniShift is NOT running, the environment variables will NOT be properly set!"
+    fi
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_API_URL=$MSHIFT
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_CONSOLE_URL=$MSHIFT
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_USERNAME=developer
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_PASSWORD=developer
+    # If set, will override username/password authentication scheme
+        #export LAUNCHER_MISSIONCONTROL_OPENSHIFT_TOKEN=<token here>
+
+    # Setup git providers default credentials (please look at README.md)
+    export LAUNCHER_MISSIONCONTROL_GITHUB_USERNAME=`git config github.user`
+    export LAUNCHER_MISSIONCONTROL_GITHUB_TOKEN=`git config github.token`
+    export LAUNCHER_MISSIONCONTROL_GITLAB_USERNAME=`git config gitlab.user`
+    export LAUNCHER_MISSIONCONTROL_GITLAB_PRIVATE_TOKEN=`git config gitlab.token`
+    export LAUNCHER_MISSIONCONTROL_BITBUCKET_USERNAME=`git config bitbucket.user`
+    export LAUNCHER_MISSIONCONTROL_BITBUCKET_APPLICATION_PASSWORD=`git config bitbucket.password`
+    ;;
+"OFFICIAL")
+    # Official KeyCloak
+    export LAUNCHER_KEYCLOAK_URL=https://sso.openshift.io/auth
+    export LAUNCHER_KEYCLOAK_REALM=rh-developers-launch
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE=$SCRIPT_DIR/clusters.yaml
+    ;;
+"LOCAL")
+    # Local KeyCloak
+    export LAUNCHER_KEYCLOAK_URL=http://localhost:8280/auth
+    export LAUNCHER_KEYCLOAK_REALM=launch
+    export LAUNCHER_MISSIONCONTROL_OPENSHIFT_CLUSTERS_FILE=$SCRIPT_DIR/clusters.yaml
+    ;;
+*)
+    echo ERROR: Failed to setup environment. Please choose a KEYCLOAK mode.
+    [ $PS1 ] && return || exit;
+    ;;
+esac
+
+# This will be set to "staging" on a staging server and "production" on a production server
+#export LAUNCHER_BACKEND_ENVIRONMENT=development
+# This will prevent boosters being downloaded at startup making development faster (default = true)
+export LAUNCHER_PREFETCH_BOOSTERS=false
+
+# For launchpad-booster-catalog-service
+#export LAUNCHER_BOOSTER_CATALOG_REPOSITORY=https://github.com/fabric8-launcher/launcher-booster-catalog.git
+#export LAUNCHER_BOOSTER_CATALOG_REF=master
+
+# For launchpad-frontend
+export LAUNCHER_MISSIONCONTROL_URL="ws://127.0.0.1:8080" #TODO needs to be default in the code (front-end)
+export LAUNCHER_BACKEND_URL="http://127.0.0.1:8080/api" #TODO needs to be default in the code (front-end)
+
+# Testing tracker token
+export LAUNCHER_TRACKER_SEGMENT_TOKEN=dMV5AjaweCpO3KZop7TuZ0961UO74AF0
+
+# For OSIO addon in the backend
+export WIT_URL=https://api.openshift.io
+export AUTH_URL=https://auth.openshift.io
+export KEYCLOAK_SAAS_URL=https://sso.openshift.io/
+export OPENSHIFT_API_URL=https://api.starter-us-east-2.openshift.com
+
+# For OSIO frontend
+export FABRIC8_FORGE_API_URL=http://localhost:8080
+
+# For Integration Tests
+export LAUNCHER_TESTS_TRUSTSTORE_PATH=${PWD}/services/git-service-impl/src/test/resources/hoverfly/hoverfly.jks
+
+# Display LAUNCHER environment
+env | grep LAUNCHER
