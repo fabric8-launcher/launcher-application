b81a71cc48359b5953ff751889c04cf63a011326
George Gastaldi <gegastaldi@gmail.com>
2018-03-01 13:05:52 +0000

    chore(git): Remove support for X-Git-Token

--- web/src/main/java/io/fabric8/launcher/web/producers/GitServiceProducer.java
+++ web/src/main/java/io/fabric8/launcher/web/producers/GitServiceProducer.java
@@ -12,7 +12,6 @@ import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.HttpHeaders;
 
 import io.fabric8.launcher.base.identity.Identity;
-import io.fabric8.launcher.base.identity.IdentityFactory;
 import io.fabric8.launcher.core.spi.IdentityProvider;
 import io.fabric8.launcher.service.git.api.GitService;
 import io.fabric8.launcher.service.git.api.GitServiceFactory;
@@ -28,8 +27,6 @@ public class GitServiceProducer {
 
     private static final String GIT_PROVIDER_HEADER = "X-Git-Provider";
 
-    private static final String GIT_TOKEN_HEADER = "X-Git-Token";
-
     @Inject
     private Instance<GitServiceFactory> gitServiceFactories;
 
@@ -41,17 +38,11 @@ public class GitServiceProducer {
     GitService getGitService(HttpServletRequest request, IdentityProvider identityProvider) {
         final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
         GitServiceFactory gitServiceFactory = getGitServiceFactory(request);
-        Identity identity;
-        String token = request.getHeader(GIT_TOKEN_HEADER);
-        if (token != null) {
-            identity = IdentityFactory.createFromToken(token);
-        } else {
-            identity = gitServiceFactory.getDefaultIdentity()
-                    .orElseGet(
-                            () -> identityProvider.getIdentity(gitServiceFactory.getName().toLowerCase(), authorization)
-                                    .orElseThrow(() -> new NotFoundException("Git token not found"))
-                    );
-        }
+        Identity identity = gitServiceFactory.getDefaultIdentity()
+                .orElseGet(
+                        () -> identityProvider.getIdentity(gitServiceFactory.getName().toLowerCase(), authorization)
+                                .orElseThrow(() -> new NotFoundException("Git token not found"))
+                );
         return gitServiceFactory.create(identity);
     }
 
