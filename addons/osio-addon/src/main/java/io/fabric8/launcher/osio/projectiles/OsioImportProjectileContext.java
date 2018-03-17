5744aac84bbb768c503722c58e0d413cdedeb88b
rratnawa <rratnawa@redhat.com>
2018-03-18 02:08:01 +0530

    add proper validation to space path

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/projectiles/OsioImportProjectileContext.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/projectiles/OsioImportProjectileContext.java
@@ -3,6 +3,7 @@ package io.fabric8.launcher.osio.projectiles;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.ws.rs.FormParam;
+import javax.validation.constraints.Size;
 
 public class OsioImportProjectileContext {
 
@@ -23,7 +24,11 @@ public class OsioImportProjectileContext {
 
     @FormParam("spacePath")
     @NotNull
-    @Pattern(message = "Space Path should start with a /", regexp = "\\/[a-zA-Z]*")
+    @Size(message = "Space Path must be in the range of 4 to 63 characters long", min = 5, max = 63)
+    @Pattern(message = "Space Path should start with a slash (/)."
+            + " must contain only letters, numbers, spaces, underscores (_) or hyphens (-)"
+            + " It cannot start or end with a space, an underscore or a hyphen.",
+            regexp = "\\/[a-zA-Z\\d][a-zA-Z\\d\\s_-]*[a-zA-Z\\d]$")
     private String spacePath;
 
     public String getGitOrganization() {
