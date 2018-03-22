9dbdec266621dd3e158afbf7df2165b9c94c31ee
ia3andy <adamevin@redhat.com>
2018-03-22 10:28:13 +0100

    refactor(osio): delegate requests to helpers

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/identity/OsioIdentityProvider.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/identity/OsioIdentityProvider.java
@@ -1,21 +1,20 @@
-package io.fabric8.launcher.osio.providers;
+package io.fabric8.launcher.osio.identity;
 
 import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.core.HttpHeaders;
 
-import io.fabric8.launcher.base.http.ExternalRequest;
 import io.fabric8.launcher.base.identity.Identity;
 import io.fabric8.launcher.base.identity.IdentityFactory;
+import io.fabric8.launcher.base.identity.TokenIdentity;
 import io.fabric8.launcher.core.spi.Application;
 import io.fabric8.launcher.core.spi.IdentityProvider;
-import io.fabric8.launcher.osio.EnvironmentVariables;
-import okhttp3.Request;
 
 import static io.fabric8.launcher.base.identity.IdentityFactory.createFromToken;
 import static io.fabric8.launcher.base.identity.IdentityHelper.removeBearerPrefix;
 import static io.fabric8.launcher.core.spi.Application.ApplicationType.OSIO;
+import static io.fabric8.launcher.osio.EnvironmentVariables.ExternalServices.getGithubServiceName;
+import static io.fabric8.launcher.osio.identity.OsioIdentityRequests.getServiceToken;
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
@@ -26,22 +25,15 @@ public class OsioIdentityProvider implements IdentityProvider {
 
     @Override
     public Optional<Identity> getIdentity(String service, String authorization) {
+        final TokenIdentity osioIdentity = createFromToken(removeBearerPrefix(authorization));
         switch (service) {
             case ServiceType.GITHUB:
-                Request gitHubTokenRequest = new Request.Builder()
-                        .url(EnvironmentVariables.ExternalServices.getGithubTokenURL())
-                        .header(HttpHeaders.AUTHORIZATION, authorization)
-                        .build();
-                return ExternalRequest.readJson(gitHubTokenRequest, tree -> tree.get("access_token").asText())
+                return getServiceToken(osioIdentity, getGithubServiceName())
                         .map(IdentityFactory::createFromToken);
             case ServiceType.OPENSHIFT:
-                return Optional.of(createFromToken(removeBearerPrefix(authorization)));
+                return Optional.of(osioIdentity);
             default:
-                Request tokenRequest = new Request.Builder()
-                        .url(EnvironmentVariables.ExternalServices.getTokenForURL() + service)
-                        .header(HttpHeaders.AUTHORIZATION, authorization)
-                        .build();
-                return ExternalRequest.readJson(tokenRequest, tree -> tree.get("access_token").asText())
+                return getServiceToken(osioIdentity, service)
                         .map(IdentityFactory::createFromToken);
         }
     }
