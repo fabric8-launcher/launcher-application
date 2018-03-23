cf6c4f60a95d1054e3b2e4ff137f30d9a5f339b0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-03-23 13:45:02 +0100

    feature(config-map): Added way to inject jenkins file snippets into pipeline

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/preparers/CreateJenkinsfilePreparer.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/preparers/CreateJenkinsfilePreparer.java
@@ -3,6 +3,8 @@ package io.fabric8.launcher.osio.preparers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -14,16 +16,19 @@ import io.fabric8.launcher.osio.jenkins.JenkinsPipeline;
 import io.fabric8.launcher.osio.jenkins.JenkinsPipelineRegistry;
 import io.fabric8.launcher.osio.projectiles.OsioProjectileContext;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 @RequestScoped
 public class CreateJenkinsfilePreparer implements ProjectilePreparer {
+    private static final Pattern INJECT_FRAGMENT = Pattern.compile("// INJECT FRAGMENT (.*)");
 
     @Inject
-    private JenkinsPipelineRegistry pipelineRegistry;
+    JenkinsPipelineRegistry pipelineRegistry;
 
     @Override
     public void prepare(Path projectPath, RhoarBooster booster, ProjectileContext genericContext) {
@@ -35,9 +40,44 @@ public class CreateJenkinsfilePreparer implements ProjectilePreparer {
                 .orElseThrow(() -> new IllegalArgumentException("Pipeline Id not found: " + context.getPipelineId()));
         Path jenkinsfilePath = jenkinsPipeline.getJenkinsfilePath();
         try {
-            Files.copy(jenkinsfilePath, projectPath.resolve(jenkinsfilePath.getFileName()), REPLACE_EXISTING);
+            byte[] content = getJenkinsPipelineContents(projectPath, jenkinsfilePath).getBytes(UTF_8);
+            Files.write(projectPath.resolve(jenkinsfilePath.getFileName()), content, CREATE, TRUNCATE_EXISTING);
         } catch (IOException e) {
             throw new IllegalStateException("Cannot copy Jenkinsfile from selected pipeline", e);
         }
+    }
+
+    private String getJenkinsPipelineContents(Path projectPath, Path jenkinsFilePath) throws IOException {
+        String jenkinsPipelineContent = readJenkinsPipelineFileContent(jenkinsFilePath);
+        String snippletName = findSnippetName(jenkinsPipelineContent);
+        String snippetsContent = findSnippet(projectPath, snippletName);
+        return mergeJenkinsSnippetsIntoPipeline(jenkinsPipelineContent, snippetsContent);
+    }
+
+    private String readJenkinsPipelineFileContent(Path jenkinsFilePath) throws IOException {
+        return convertBytesToString(Files.readAllBytes(jenkinsFilePath));
+    }
+
+    private String findSnippetName(String content) {
+        Matcher matcher = INJECT_FRAGMENT.matcher(content);
+        return matcher.find() ? matcher.group(1) : "none";
+    }
+
+    private String findSnippet(Path projectPath, String name) throws IOException {
+        Path snippetsFolder = projectPath.resolve(".openshiftio");
+        Path jenkinsSnippet = snippetsFolder.resolve(String.format("Jenkinsfile.%s.snippet", name));
+
+        if (jenkinsSnippet.toFile().exists()) {
+            return convertBytesToString(Files.readAllBytes(jenkinsSnippet));
+        }
+        return "";
+    }
+
+    private String mergeJenkinsSnippetsIntoPipeline(String pipelineContent, String snippetsContent) {
+        return INJECT_FRAGMENT.matcher(pipelineContent).replaceAll(snippetsContent.replace("$", "\\$"));
+    }
+
+    private String convertBytesToString(byte[] bytes) {
+        return new String(bytes, UTF_8);
     }
 }
---
+++ addons/osio-addon/src/test/java/io/fabric8/launcher/osio/preparers/CreateJenkinsfilePreparerTest.java
@@ -0,0 +1,128 @@
+package io.fabric8.launcher.osio.preparers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import io.fabric8.launcher.osio.jenkins.ImmutableJenkinsPipeline;
+import io.fabric8.launcher.osio.jenkins.JenkinsPipeline;
+import io.fabric8.launcher.osio.jenkins.JenkinsPipelineRegistry;
+import io.fabric8.launcher.osio.projectiles.OsioProjectileContext;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CreateJenkinsfilePreparerTest {
+
+    private static final String PIPELINE_ID = "test";
+
+    private CreateJenkinsfilePreparer preparer = new CreateJenkinsfilePreparer();
+
+    private Path basePath;
+
+    private JenkinsPipeline jenkinsPipeline;
+
+    @Before
+    public void init() throws URISyntaxException {
+        basePath = Paths.get(getClass().getResource("/jenkinsfile-preparer-test").toURI());
+        preparer.pipelineRegistry = new JenkinsPipelineRegistry() {
+            @Override
+            public Optional<JenkinsPipeline> findPipelineById(String pipelineId) {
+                return Optional.of(jenkinsPipeline);
+            }
+        };
+    }
+
+    @Test
+    public void shouldMergeJenkinsSnippetsIntoJenkinsPipelineFile() throws IOException {
+        // given
+        Path pipelinePath = getJenkinsFile();
+        Path testProject1 = basePath.resolve("test-project1");
+
+        // when
+        preparer.prepare(testProject1, null, getOsioProjectileContext());
+
+        // then
+        assertJenkinsFileEquals(pipelinePath, "Jenkinsfile-result", testProject1);
+    }
+
+    @Test
+    public void shouldMergeWhenNoInjectionPoint() throws IOException {
+        // given
+        Path jenkinsFile = basePath.resolve("pipeline").resolve("no-inject").resolve("Jenkinsfile");
+        initPipeline(jenkinsFile);
+
+        Path testProject1 = basePath.resolve("test-project1");
+
+        // when
+        preparer.prepare(testProject1, null, getOsioProjectileContext());
+
+        // then
+        Path result = testProject1.resolve("Jenkinsfile");
+        assertEquals(Files.readAllLines(jenkinsFile), Files.readAllLines(result));
+    }
+
+    @Test
+    public void shouldMergeWithoutSnippets() throws IOException {
+        // given
+        Path pipelinePath = getJenkinsFile();
+        Path testProject2 = basePath.resolve("test-project2");
+
+        // when
+        preparer.prepare(testProject2, null, getOsioProjectileContext());
+
+        // then
+        assertJenkinsFileEquals(pipelinePath, "Jenkinsfile-result-empty", testProject2);
+    }
+
+    @Test
+    public void shouldOverwriteExistingJenkinsFile() throws IOException {
+        // given
+        Path pipelinePath = getJenkinsFile();
+        Path testProject3 = basePath.resolve("test-project3");
+
+        // when
+        preparer.prepare(testProject3, null, getOsioProjectileContext());
+
+        // then
+        assertJenkinsFileEquals(pipelinePath, "Jenkinsfile-result-empty", testProject3);
+    }
+
+    private void assertJenkinsFileEquals(Path pipelinePath, String name, Path projectPath) throws IOException {
+        Path expectedResultFile = pipelinePath.resolve(name);
+        Path result = projectPath.resolve("Jenkinsfile");
+        assertEquals(Files.readAllLines(expectedResultFile), Files.readAllLines(result));
+    }
+
+    private OsioProjectileContext getOsioProjectileContext() {
+        OsioProjectileContext context = mock(OsioProjectileContext.class);
+        when(context.getPipelineId()).thenReturn(PIPELINE_ID);
+        return context;
+    }
+
+    private Path getJenkinsFile() {
+        Path pipelinePath = basePath.resolve("pipeline").resolve("inject");
+        Path jenkinsFile = pipelinePath.resolve("Jenkinsfile");
+        initPipeline(jenkinsFile);
+        return pipelinePath;
+    }
+
+    private void initPipeline(Path jenkinsFile) {
+        this.jenkinsPipeline = ImmutableJenkinsPipeline.builder()
+                .platform("osio")
+                .name("release")
+                .description("")
+                .isSuggested(false)
+                .isTechPreview(false)
+                .jenkinsfilePath(jenkinsFile)
+                .id(PIPELINE_ID)
+                .build();
+    }
+
+}
