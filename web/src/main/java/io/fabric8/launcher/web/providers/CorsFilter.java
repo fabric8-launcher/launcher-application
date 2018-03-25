a4a0c05f73364628de8c3308f9e31dc343ce3271
George Gastaldi <gegastaldi@gmail.com>
2018-03-24 23:26:55 -0300

    fix(cors): CORS Filter now works in Firefox
    Fixes #222

--- web/src/main/java/io/fabric8/launcher/web/providers/CorsFilter.java
+++ web/src/main/java/io/fabric8/launcher/web/providers/CorsFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -19,31 +20,52 @@ import javax.servlet.http.HttpServletResponse;
 @WebFilter(filterName = "CorsFilter", urlPatterns = "/*", asyncSupported = true)
 public class CorsFilter implements Filter {
 
-    private static final String ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS, HEAD";
+    private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 
-    private static final int MAX_AGE = 42 * 60 * 60;
+    private static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
 
-    private static final String DEFAULT_ALLOWED_HEADERS = "*";
+    private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
 
-    private static final String DEFAULT_EXPOSED_HEADERS = "*";
+    private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+    private static final String ORIGIN = "Origin";
+
+    private static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+
+    private static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+
+    private static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
 
     @Override
     public void init(FilterConfig filterConfig) {
-
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", DEFAULT_ALLOWED_HEADERS);
-        httpServletResponse.addHeader("Access-Control-Allow-Headers", DEFAULT_ALLOWED_HEADERS);
-        httpServletResponse.addHeader("Access-Control-Expose-Headers", DEFAULT_EXPOSED_HEADERS);
-        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
-        httpServletResponse.addHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
-        httpServletResponse.addIntHeader("Access-Control-Max-Age", MAX_AGE);
-        chain.doFilter(request, response);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String origin = request.getHeader(ORIGIN);
+        if (origin == null) {
+            chain.doFilter(servletRequest, servletResponse);
+        } else {
+            String requestMethods = request.getHeader(ACCESS_CONTROL_REQUEST_METHOD);
+            if (requestMethods != null) {
+                response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, requestMethods);
+            }
+            String allowHeaders = request.getHeader(ACCESS_CONTROL_REQUEST_HEADERS);
+            if (allowHeaders != null) {
+                response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, allowHeaders);
+            }
+            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+            response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+            response.setHeader(ACCESS_CONTROL_EXPOSE_HEADERS, "Content-Disposition");
+            if (!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
+                chain.doFilter(servletRequest, servletResponse);
+            } else {
+                response.flushBuffer();
+            }
+        }
     }
-
 
     @Override
     public void destroy() {
