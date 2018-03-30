da2171855ea2f3bd2085d6826c34008069d2a4f7
ia3andy <adamevin@redhat.com>
2018-03-30 16:20:21 +0200

    refactor(identity): Identity header authorization lazy for perf

--- base/src/main/java/io/fabric8/launcher/base/identity/Identity.java
+++ base/src/main/java/io/fabric8/launcher/base/identity/Identity.java
@@ -2,6 +2,8 @@ package io.fabric8.launcher.base.identity;
 
 import java.util.function.Consumer;
 
+import org.immutables.value.Value;
+
 import static io.fabric8.launcher.base.identity.Identities.createRequestAuthorizationHeaderKey;
 import static io.fabric8.launcher.base.identity.Identities.createRequestAuthorizationHeaderValue;
 
@@ -12,10 +14,12 @@ import static io.fabric8.launcher.base.identity.Identities.createRequestAuthoriz
  */
 public interface Identity extends Consumer<IdentityVisitor> {
 
+    @Value.Lazy
     default String toRequestHeaderKey() {
         return createRequestAuthorizationHeaderKey(this);
     }
 
+    @Value.Lazy
     default String toRequestHeaderValue() {
         return createRequestAuthorizationHeaderValue(this);
     }
