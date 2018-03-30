59bcc15fd2062f3f785f7dc9e2afc7bb5b944037
ia3andy <adamevin@redhat.com>
2018-03-30 17:13:40 +0200

    test(osio): Add Hoverfly tests for all osio requests
    Fixes #219

---
+++ addons/osio-addon/src/test/java/io/fabric8/launcher/osio/client/OsioAuthClientTest.java
@@ -0,0 +1,55 @@
+package io.fabric8.launcher.osio.client;
+
+import java.util.Optional;
+
+import io.fabric8.launcher.base.identity.Identity;
+import io.fabric8.launcher.base.identity.TokenIdentity;
+import io.fabric8.launcher.base.test.hoverfly.LauncherPerTestHoverflyRule;
+import io.fabric8.launcher.core.spi.IdentityProvider;
+import io.fabric8.launcher.osio.client.api.OsioAuthClient;
+import io.fabric8.launcher.osio.client.api.OsioJenkinsClient;
+import io.fabric8.launcher.osio.client.impl.OsioAuthClientImpl;
+import io.fabric8.launcher.osio.client.impl.OsioJenkinsClientImpl;
+import io.specto.hoverfly.junit.rule.HoverflyRule;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+
+import static io.fabric8.launcher.base.test.hoverfly.LauncherHoverflyEnvironment.createDefaultHoverflyEnvironment;
+import static io.fabric8.launcher.base.test.hoverfly.LauncherHoverflyRuleConfigurer.createMultiTestHoverflyProxy;
+import static io.fabric8.launcher.osio.client.OsioTests.LAUNCHER_OSIO_TOKEN;
+
+public class OsioAuthClientTest {
+
+    private static final HoverflyRule HOVERFLY_RULE = createMultiTestHoverflyProxy("auth.openshift.io|auth.prod-preview.openshift.io");
+
+    @ClassRule
+    public static final RuleChain RULE_CHAIN = RuleChain// After recording on a real environment against a real service,
+            // You should adapt the Hoverfly descriptors (.json) to make them work in simulation mode with the mock environment.
+            .outerRule(createDefaultHoverflyEnvironment(HOVERFLY_RULE)
+                .andForSimulationOnly(LAUNCHER_OSIO_TOKEN, "jneoufze937973HFRH"))
+            .around(HOVERFLY_RULE);
+
+    @Rule
+    public LauncherPerTestHoverflyRule hoverflyPerTestRule = new LauncherPerTestHoverflyRule(HOVERFLY_RULE);
+
+    @Rule
+    public JUnitSoftAssertions softly = new JUnitSoftAssertions();
+
+
+    private OsioAuthClient getOsioAuthClient(){
+        return  new OsioAuthClientImpl(OsioTests.getTestAuthorization());
+    }
+
+    @Test
+    public void shouldProvideIdentityCorrectly() {
+        final Optional<String> gitToken = getOsioAuthClient().getTokenForService("https://github.com");
+        Assertions.assertThat(gitToken)
+                .isPresent()
+                .contains("20234c2a7c51348cad0aa4fb853e7c65957b79b4");
+    }
+
+}
