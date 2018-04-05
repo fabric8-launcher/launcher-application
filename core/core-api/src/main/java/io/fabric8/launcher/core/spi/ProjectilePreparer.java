9e1057ab712c49b0a491ee3701a7ef758f3c38ae
George Gastaldi <gegastaldi@gmail.com>
2018-04-05 01:31:13 +0000

    chore: add @Nullable to RhoarBooster in ProjectilePreparer#prepare

--- core/core-api/src/main/java/io/fabric8/launcher/core/spi/ProjectilePreparer.java
+++ core/core-api/src/main/java/io/fabric8/launcher/core/spi/ProjectilePreparer.java
@@ -2,9 +2,11 @@ package io.fabric8.launcher.core.spi;
 
 import java.nio.file.Path;
 
+import javax.annotation.Nullable;
+
 import io.fabric8.launcher.booster.catalog.rhoar.RhoarBooster;
-import io.fabric8.launcher.core.api.ProjectileContext;
 import io.fabric8.launcher.core.api.Projectile;
+import io.fabric8.launcher.core.api.ProjectileContext;
 
 /**
  * Prepares the copied booster before converting into a {@link Projectile}
@@ -15,8 +17,8 @@ public interface ProjectilePreparer {
 
     /**
      * @param projectPath the project {@link Path} where the requested {@link RhoarBooster} was copied to
-     * @param booster     the {@link RhoarBooster} used in this request
+     * @param booster     the {@link RhoarBooster} used in this request. May be null in some cases
      * @param context     information from the UI
      */
-    void prepare(Path projectPath, RhoarBooster booster, ProjectileContext context);
+    void prepare(Path projectPath, @Nullable RhoarBooster booster, ProjectileContext context);
 }
