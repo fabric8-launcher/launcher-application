fa89633ac550624edace1b0fb8f9be3b82f62c5f
George Gastaldi <gegastaldi@gmail.com>
2018-04-06 12:45:17 +0000

    Revert "feature(config-map): multiple inject points by name"
    This reverts commit 5837afd3dc46e97ce0223a3bf4f388b6a59a5c83.
    
    Revert "feature(config-map): Added way to inject jenkins file snippets into pipeline"
    
    This reverts commit cf6c4f60a95d1054e3b2e4ff137f30d9a5f339b0.

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/preparers/CreateJenkinsfilePreparer.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/preparers/CreateJenkinsfilePreparer.java
@@ -3,10 +3,6 @@ package io.fabric8.launcher.osio.preparers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -18,21 +14,16 @@ import io.fabric8.launcher.osio.jenkins.JenkinsPipeline;
 import io.fabric8.launcher.osio.jenkins.JenkinsPipelineRegistry;
 import io.fabric8.launcher.osio.projectiles.context.OsioImportProjectileContext;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 @ApplicationScoped
 public class CreateJenkinsfilePreparer implements ProjectilePreparer {
-    private static final String INJECT_FRAGMENT_PART = "// INJECT FRAGMENT ";
-
-    private static final Pattern INJECT_FRAGMENT = Pattern.compile(INJECT_FRAGMENT_PART + "(.*)");
 
     @Inject
-    JenkinsPipelineRegistry pipelineRegistry;
+    private JenkinsPipelineRegistry pipelineRegistry;
 
     @Override
     public void prepare(Path projectPath, RhoarBooster booster, ProjectileContext genericContext) {
@@ -44,54 +35,9 @@ public class CreateJenkinsfilePreparer implements ProjectilePreparer {
                 .orElseThrow(() -> new IllegalArgumentException("Pipeline Id not found: " + context.getPipelineId()));
         Path jenkinsfilePath = jenkinsPipeline.getJenkinsfilePath();
         try {
-            byte[] content = getJenkinsPipelineContents(projectPath, jenkinsfilePath).getBytes(UTF_8);
-            Files.write(projectPath.resolve(jenkinsfilePath.getFileName()), content, CREATE, TRUNCATE_EXISTING);
+            Files.copy(jenkinsfilePath, projectPath.resolve(jenkinsfilePath.getFileName()), REPLACE_EXISTING);
         } catch (IOException e) {
             throw new IllegalStateException("Cannot copy Jenkinsfile from selected pipeline", e);
         }
-    }
-
-    private String getJenkinsPipelineContents(Path projectPath, Path jenkinsFilePath) throws IOException {
-        String jenkinsPipelineContent = readJenkinsPipelineFileContent(jenkinsFilePath);
-        List<String> snippetNames = findSnippetNames(jenkinsPipelineContent);
-
-        for (String snippetName : snippetNames) {
-            String snippetsContent = findSnippet(projectPath, snippetName);
-            jenkinsPipelineContent = mergeJenkinsSnippetIntoPipeline(jenkinsPipelineContent, snippetName, snippetsContent);
-        }
-        return jenkinsPipelineContent;
-    }
-
-    private String readJenkinsPipelineFileContent(Path jenkinsFilePath) throws IOException {
-        return convertBytesToString(Files.readAllBytes(jenkinsFilePath));
-    }
-
-    private List<String> findSnippetNames(String content) {
-        List<String> result = new ArrayList<>();
-        Matcher matcher = INJECT_FRAGMENT.matcher(content);
-        while (matcher.find()) {
-            result.add(matcher.group(1));
-        }
-
-        return result;
-    }
-
-    private String findSnippet(Path projectPath, String name) throws IOException {
-        Path snippetsFolder = projectPath.resolve(".openshiftio");
-        Path jenkinsSnippet = snippetsFolder.resolve(String.format("Jenkinsfile.%s.snippet", name));
-
-        if (jenkinsSnippet.toFile().exists()) {
-            return convertBytesToString(Files.readAllBytes(jenkinsSnippet));
-        }
-        return "";
-    }
-
-    private String mergeJenkinsSnippetIntoPipeline(String pipelineContent, String snippetName, String snippetsContent) {
-        return Pattern.compile(INJECT_FRAGMENT_PART + snippetName).matcher(pipelineContent).replaceAll(
-                snippetsContent.replace("$", "\\$"));
-    }
-
-    private String convertBytesToString(byte[] bytes) {
-        return new String(bytes, UTF_8);
     }
 }
