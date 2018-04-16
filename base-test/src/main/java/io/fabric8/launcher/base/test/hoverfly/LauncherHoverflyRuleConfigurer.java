3d8c5aec30efeb8077553270b7b10bd7c9abbdde
ia3andy <adamevin@redhat.com>
2018-04-16 16:03:37 +0200

    test(hoverfly): Change LAUNCHER_TESTS_SV_SIMULATION environment variable
    IMO, our mindset when we use this variable is "I want to capture hoverfly files" -> `LAUNCHER_HOVERFLY_CAPTURE`
    
    BREAKING CHANGE:
    
    To activate capure mode on Hoverfly tests, now use LAUNCHER_HOVERFLY_CAPTURE=true instead of LAUNCHER_TESTS_SV_SIMULATION=false
    
    Closes #319

--- base-test/src/main/java/io/fabric8/launcher/base/test/hoverfly/LauncherHoverflyRuleConfigurer.java
+++ base-test/src/main/java/io/fabric8/launcher/base/test/hoverfly/LauncherHoverflyRuleConfigurer.java
@@ -12,7 +12,7 @@ public final class LauncherHoverflyRuleConfigurer {
         throw new IllegalAccessError("Utility class");
     }
 
-    private static final String LAUNCHER_TESTS_SV_SIMULATION = "LAUNCHER_TESTS_SV_SIMULATION";
+    private static final String LAUNCHER_HOVERFLY_CAPTURE = "LAUNCHER_HOVERFLY_CAPTURE";
 
     /**
      * Use random available port
@@ -30,8 +30,8 @@ public final class LauncherHoverflyRuleConfigurer {
 
     /**
      * Creates service virtualization layer through mitm proxy for 3rd party API interaction.
-     * If not specified otherwise by using LAUNCHER_TESTS_SV_SIMULATION env variable or system property
-     * it will be running in the simulation mode.
+     * If not specified otherwise by using LAUNCHER_HOVERFLY_CAPTURE env variable or system property
+     * it will be running in the capture mode.
      *
      * Otherwise calls are proxied to the actual GitHub endpoint and captured afterwards. This way we can use
      * recorded calls against real service to update simulation in case of logic on either side has changes (ours: test
@@ -59,7 +59,11 @@ public final class LauncherHoverflyRuleConfigurer {
     }
 
     public static boolean isHoverflyInSimulationMode() {
-        return EnvironmentSupport.INSTANCE.getBooleanEnvVarOrSysProp(LAUNCHER_TESTS_SV_SIMULATION, true);
+        return !isHoverflyInCaptureMode();
+    }
+
+    public static boolean isHoverflyInCaptureMode() {
+        return EnvironmentSupport.INSTANCE.getBooleanEnvVarOrSysProp(LAUNCHER_HOVERFLY_CAPTURE, false);
     }
 
 }
