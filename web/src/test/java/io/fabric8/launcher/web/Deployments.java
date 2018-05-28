ae6a0c292d6694255fa4f8450402f8617cb556ee
George Gastaldi <gegastaldi@gmail.com>
2018-05-28 19:16:12 +0000

    fix(test): BaseResourceIT should follow the beans.xml in web

--- web/src/test/java/io/fabric8/launcher/web/Deployments.java
+++ web/src/test/java/io/fabric8/launcher/web/Deployments.java
@@ -4,7 +4,7 @@ import javax.enterprise.inject.spi.Extension;
 
 import io.fabric8.launcher.web.forge.cdi.LauncherExtension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
@@ -20,10 +20,10 @@ public class Deployments {
      */
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class)
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"annotated\" version=\"1.1\"/>"),
+                                     "beans.xml")
                 .addAsServiceProvider(Extension.class, LauncherExtension.class)
-                .addPackages(true,
-                             PackageMarker.class.getPackage())
+                .addPackages(true, PackageMarker.class.getPackage())
                 .addAsLibraries(Maven.resolver()
                                         .loadPomFromFile("pom.xml")
                                         .importCompileAndRuntimeDependencies()
