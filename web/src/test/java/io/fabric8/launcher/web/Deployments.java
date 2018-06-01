cbad3957914d844cb7822a3cbad2ef3757de4b2b
George Gastaldi <gegastaldi@gmail.com>
2018-06-01 09:34:15 -0300

    refactor: Remove launcher-addon (#415)
    BREAKING CHANGE: This also removes the Forge dependencies and some unused fractions
    Front-ends using the legacy UI will no longer work
    
    Signed-off-by: George Gastaldi <gegastaldi@gmail.com>

--- web/src/test/java/io/fabric8/launcher/web/Deployments.java
+++ web/src/test/java/io/fabric8/launcher/web/Deployments.java
@@ -1,8 +1,5 @@
 package io.fabric8.launcher.web;
 
-import javax.enterprise.inject.spi.Extension;
-
-import io.fabric8.launcher.web.forge.cdi.LauncherExtension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -22,7 +19,6 @@ public class Deployments {
         return ShrinkWrap.create(WebArchive.class)
                 .addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"annotated\" version=\"1.1\"/>"),
                                      "beans.xml")
-                .addAsServiceProvider(Extension.class, LauncherExtension.class)
                 .addPackages(true, PackageMarker.class.getPackage())
                 .addAsLibraries(Maven.resolver()
                                         .loadPomFromFile("pom.xml")
