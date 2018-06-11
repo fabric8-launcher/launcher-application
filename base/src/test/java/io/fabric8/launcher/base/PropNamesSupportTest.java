8c8f080856a300338d94a7ae8620e0a0bd6e1d03
George Gastaldi <gegastaldi@gmail.com>
2018-06-11 18:38:59 -0300

    test(base): Add test case for PropNamesSupport

---
+++ base/src/test/java/io/fabric8/launcher/base/PropNamesSupportTest.java
@@ -0,0 +1,41 @@
+package io.fabric8.launcher.base;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class PropNamesSupportTest {
+
+    @Test
+    public void should_return_env_var() {
+        assertThat(TestEnum.HOSTNAME.value()).isEqualTo(System.getenv("HOSTNAME"));
+    }
+
+    @Test
+    public void should_return_property() {
+        assertThat(TestEnum.JAVA_VERSION.value()).isEqualTo(System.getProperty("java.version"));
+    }
+
+    private enum TestEnum implements PropNamesSupport {
+        HOSTNAME,
+        JAVA_VERSION("java.version");
+
+        private final String propertyKey;
+
+        TestEnum() {
+            this.propertyKey = name();
+        }
+
+        TestEnum(String propertyKey) {
+            this.propertyKey = propertyKey;
+        }
+
+        @Override
+        public String propertyKey() {
+            return propertyKey;
+        }
+    }
+}
