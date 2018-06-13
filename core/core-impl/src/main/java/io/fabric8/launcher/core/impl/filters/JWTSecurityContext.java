7df53eb931041ecc8ed19841552377a2b484611b
George Gastaldi <gegastaldi@gmail.com>
2018-06-13 18:47:51 -0300

    feat(core): SecuredContext is now set in SecuredFilter

---
+++ core/core-impl/src/main/java/io/fabric8/launcher/core/impl/filters/JWTSecurityContext.java
@@ -0,0 +1,57 @@
+package io.fabric8.launcher.core.impl.filters;
+
+import java.security.Principal;
+
+import javax.ws.rs.core.SecurityContext;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class JWTSecurityContext implements SecurityContext {
+
+    private final DecodedJWT jwt;
+
+    private final JWTPrincipal principal;
+
+    JWTSecurityContext(DecodedJWT jwt) {
+        this.jwt = jwt;
+        this.principal = new JWTPrincipal(jwt.getClaim("preferred_username").asString());
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return principal;
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        return false;
+    }
+
+    @Override
+    public boolean isSecure() {
+        return false;
+    }
+
+    @Override
+    public String getAuthenticationScheme() {
+        return "JWT";
+    }
+
+
+    private class JWTPrincipal implements Principal {
+
+        private final String name;
+
+        private JWTPrincipal(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
+}
