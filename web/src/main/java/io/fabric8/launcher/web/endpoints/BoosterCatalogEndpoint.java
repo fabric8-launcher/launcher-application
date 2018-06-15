7076676db8e95bfe908e376e5c33047c05fe423a
George Gastaldi <gegastaldi@gmail.com>
2018-06-15 16:39:27 +0000

    refactor(booster-catalog): remove deprecated endpoints

--- web/src/main/java/io/fabric8/launcher/web/endpoints/BoosterCatalogEndpoint.java
+++ web/src/main/java/io/fabric8/launcher/web/endpoints/BoosterCatalogEndpoint.java
@@ -2,7 +2,6 @@ package io.fabric8.launcher.web.endpoints;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Predicate;
 
@@ -10,11 +9,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -35,9 +32,7 @@ import io.fabric8.launcher.core.api.catalog.BoosterCatalogFactory;
 
 import static io.fabric8.launcher.base.JsonUtils.toJsonObjectBuilder;
 import static io.fabric8.launcher.booster.catalog.rhoar.BoosterPredicates.withAppEnabled;
-import static io.fabric8.launcher.booster.catalog.rhoar.BoosterPredicates.withMission;
 import static io.fabric8.launcher.booster.catalog.rhoar.BoosterPredicates.withParameters;
-import static io.fabric8.launcher.booster.catalog.rhoar.BoosterPredicates.withRunsOn;
 import static io.fabric8.launcher.booster.catalog.rhoar.BoosterPredicates.withRuntime;
 import static javax.json.Json.createArrayBuilder;
 import static javax.json.Json.createObjectBuilder;
@@ -53,134 +48,6 @@ public class BoosterCatalogEndpoint {
 
     @Inject
     private BoosterCatalogFactory boosterCatalogFactory;
-
-    /**
-     * @deprecated replaced by {@link BoosterCatalogEndpoint#getCatalog(String, UriInfo)}
-     */
-    @GET
-    @Path("/missions")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Deprecated
-    public Response getMissions(@HeaderParam(HEADER_APP) String application, @QueryParam("runsOn") String runsOn, @Context UriInfo uriInfo) {
-        MultivaluedMap<String, String> parameters = getQueryParameters(uriInfo);
-        RhoarBoosterCatalog catalog = boosterCatalogFactory.getBoosterCatalog();
-        Predicate<RhoarBooster> filter = withAppEnabled(application).and(withRunsOn(runsOn)).and(withParameters(parameters));
-        JsonArrayBuilder response = createArrayBuilder();
-        for (Mission m : catalog.getMissions(filter)) {
-            JsonArrayBuilder runtimes = createArrayBuilder();
-            JsonObjectBuilder mission = createObjectBuilder()
-                    .add("id", m.getId())
-                    .add("name", m.getName());
-            if (m.getDescription() != null) {
-                mission.add("description", m.getDescription()); //NOSONAR
-            }
-            if (!m.getMetadata().isEmpty()) {
-                mission.add("metadata", toJsonObjectBuilder(m.getMetadata())); //NOSONAR
-            }
-
-            // Add all runtimes
-            catalog.getRuntimes(filter.and(withMission(m)))
-                    .stream()
-                    .map(Runtime::getId)
-                    .forEach(runtimes::add);
-
-            mission.add("runtimes", runtimes);
-            response.add(mission);
-        }
-        return Response.ok(response.build()).build();
-    }
-
-    /**
-     * @deprecated replaced by {@link BoosterCatalogEndpoint#getCatalog(String, UriInfo)}
-     */
-    @GET
-    @Path("/runtimes")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Deprecated
-    @SuppressWarnings("squid:S3776")
-    public Response getRuntime(@HeaderParam(HEADER_APP) String application, @QueryParam("runsOn") String runsOn, @Context UriInfo uriInfo) {
-        MultivaluedMap<String, String> parameters = getQueryParameters(uriInfo);
-        RhoarBoosterCatalog catalog = boosterCatalogFactory.getBoosterCatalog();
-        Predicate<RhoarBooster> filter = withAppEnabled(application).and(withRunsOn(runsOn)).and(withParameters(parameters));
-        JsonArrayBuilder response = createArrayBuilder();
-        for (Runtime r : catalog.getRuntimes(filter)) {
-            JsonArrayBuilder missions = createArrayBuilder();
-            JsonObjectBuilder runtime = createObjectBuilder()
-                    .add("id", r.getId())
-                    .add("name", r.getName())
-                    .add("icon", r.getIcon());
-            if (r.getDescription() != null) {
-                runtime.add("description", r.getDescription());
-            }
-            if (!r.getMetadata().isEmpty()) {
-                runtime.add("metadata", toJsonObjectBuilder(r.getMetadata()));
-            }
-            for (Mission m : catalog.getMissions(filter.and(withRuntime(r)))) {
-                JsonArrayBuilder versions = createArrayBuilder();
-                JsonObjectBuilder mission = createObjectBuilder()
-                        .add("id", m.getId());
-                for (Version v : catalog.getVersions(filter.and(withMission(m)).and(withRuntime(r)))) {
-                    JsonObjectBuilder version = createObjectBuilder()
-                            .add("id", v.getId())
-                            .add("name", v.getName());
-                    if (v.getDescription() != null) {
-                        version.add("description", v.getDescription());
-                    }
-                    if (v.getMetadata() != null && !v.getMetadata().isEmpty()) {
-                        version.add("metadata", toJsonObjectBuilder(v.getMetadata()));
-                    }
-                    catalog.getBooster(m, r, v).ifPresent(booster -> {
-                        JsonObjectBuilder boosterBuilder = createObjectBuilder();
-                        if (!booster.getMetadata().isEmpty()) {
-                            boosterBuilder.add("metadata", toJsonObjectBuilder(booster.getMetadata()));
-                        }
-                        version.add("booster", boosterBuilder);
-                    });
-                    versions.add(version);
-                }
-                mission.add("versions", versions);
-                missions.add(mission);
-            }
-            runtime.add("missions", missions);
-            response.add(runtime);
-        }
-        return Response.ok(response.build()).build();
-    }
-
-    /**
-     * @deprecated replaced by {@link BoosterCatalogEndpoint#getCatalog(String, UriInfo)}
-     */
-    @GET
-    @Path("/booster")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Deprecated
-    public Response getBoosters(@NotNull(message = "mission is required") @QueryParam("mission") Mission mission,
-                                @NotNull(message = "runtime is required") @QueryParam("runtime") Runtime runtime,
-                                @QueryParam("runtimeVersion") Version version) {
-        RhoarBoosterCatalog catalog = boosterCatalogFactory.getBoosterCatalog();
-        Optional<RhoarBooster> result = catalog.getBooster(mission, runtime, version);
-
-        return result.map(b -> {
-            JsonObjectBuilder booster = createObjectBuilder()
-                    .add("id", b.getId());
-
-            booster.add("gitRepo", b.getGitRepo());
-            booster.add("gitRef", b.getGitRef());
-
-            if (!b.getMetadata().isEmpty()) {
-                booster.add("metadata", toJsonObjectBuilder(b.getMetadata()));
-            }
-
-            if (b.getMission() != null && !b.getMission().getMetadata().isEmpty()) {
-                booster.add("mission", createObjectBuilder().add("metadata", toJsonObjectBuilder(b.getMission().getMetadata())));
-            }
-
-            if (b.getRuntime() != null && !b.getRuntime().getMetadata().isEmpty()) {
-                booster.add("runtime", createObjectBuilder().add("metadata", toJsonObjectBuilder(b.getRuntime().getMetadata())));
-            }
-            return Response.ok(booster.build()).build();
-        }).orElseThrow(NotFoundException::new);
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
