5258a7b850f815c76b483c3fd63cc271c51dedeb
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-07-30 15:13:00 +0200

    fix(test): adds default URL for analytics endpoints

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/OsioConfigs.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/OsioConfigs.java
@@ -39,7 +39,7 @@ public final class OsioConfigs {
 
     private static final OpenShiftCluster OSIO_CLUSTER = new OpenShiftCluster("osio", "OpenShift.io", "osio", OPENSHIFT_API_URL, OPENSHIFT_API_URL);
 
-    private static final String ANALYTICS_URL = getEnvVarOrSysProp("F8A_ANALYTICS_RECOMMENDER_API_URL");
+    private static final String ANALYTICS_URL = getEnvVarOrSysProp("F8A_ANALYTICS_RECOMMENDER_API_URL", "https://recommender.api.openshift.io");
 
     public static String getWitUrl() {
         return WIT_URL;
