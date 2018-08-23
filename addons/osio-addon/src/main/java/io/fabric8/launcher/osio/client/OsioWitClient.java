b9c9c2fbadbba5235a91d064f2dda2ea7e6cd293
tinakurian <tkurian@redhat.com>
2018-08-23 08:12:35 -0400

    re-working file

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/client/OsioWitClient.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/client/OsioWitClient.java
@@ -1,19 +1,9 @@
 package io.fabric8.launcher.osio.client;
 
-import static io.fabric8.launcher.base.http.Requests.securedRequest;
-import static io.fabric8.launcher.base.http.Requests.urlEncode;
-import static io.fabric8.launcher.osio.OsioConfigs.getAuthUrl;
-import static io.fabric8.launcher.osio.OsioConfigs.getWitUrl;
-import static io.fabric8.utils.URLUtils.pathJoin;
-import static java.util.Objects.requireNonNull;
-import static okhttp3.MediaType.parse;
-import static okhttp3.RequestBody.create;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,24 +14,31 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
 import io.fabric8.launcher.base.JsonUtils;
 import io.fabric8.launcher.base.http.HttpClient;
 import io.fabric8.launcher.base.http.HttpException;
 import io.fabric8.launcher.base.identity.TokenIdentity;
 import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.ResponseBody;
+
+import static io.fabric8.launcher.base.http.Requests.securedRequest;
+import static io.fabric8.launcher.base.http.Requests.urlEncode;
+import static io.fabric8.launcher.osio.OsioConfigs.getAuthUrl;
+import static io.fabric8.launcher.osio.OsioConfigs.getWitUrl;
+import static io.fabric8.utils.URLUtils.pathJoin;
+import static java.util.Objects.requireNonNull;
+import static okhttp3.MediaType.parse;
+import static okhttp3.RequestBody.create;
 
 /**
  * Client to request Osio wit api
  */
 @RequestScoped
 public class OsioWitClient {
-
 	protected static final String ERROR_HTTP_CLIENT_MUST_BE_SPECIFIED = "httpClient must be specified"; //$NON-NLS-1$
 	protected static final String ERROR_AUTHORIZATION_MUST_BE_SPECIFIED = "authorization must be specified."; //$NON-NLS-1$
 
+	private static final String ERROR_RETRIEVING_DECODED_STRING_BODY = "Error retrieving decoded string body:"; //$NON-NLS-1$
 	private static final String ERROR_NAMESPACES_NOT_FOUND = "Namespaces not found"; //$NON-NLS-1$
 	private static final String ERROR_USER_INFO_NOT_FOUND = "UserInfo not found"; //$NON-NLS-1$
 	private static final String ERROR_CREATING_SPACE = "Error while creating space with name:"; //$NON-NLS-1$
@@ -54,6 +51,8 @@ public class OsioWitClient {
 
 	private static final String USERNAME = "username"; //$NON-NLS-1$
 	private static final String CLUSTER = "cluster"; //$NON-NLS-1$
+	private static final String CLUSTER_CONSOLE_URL = "cluster-console-url"; //$NON-NLS-1$
+	private static final String CLUSTER_URL = "cluster-url"; //$NON-NLS-1$
 	private static final String EMAIL = "email"; //$NON-NLS-1$
 	private static final String NAMESPACES = "namespaces"; //$NON-NLS-1$
 	private static final String TYPE = "type"; //$NON-NLS-1$
@@ -61,8 +60,6 @@ public class OsioWitClient {
 	private static final String NAME = "name"; //$NON-NLS-1$
 	private static final String ATTRIBUTES = "attributes"; //$NON-NLS-1$
 	private static final String DATA = "data"; //$NON-NLS-1$
-	private static final String DETAIL = "detail"; //$NON-NLS-1$
-	private static final String ERRORS = "errors"; //$NON-NLS-1$
 
 	private static final Logger logger = Logger.getLogger(OsioWitClient.class.getName());
 
@@ -70,8 +67,8 @@ public class OsioWitClient {
 	private final HttpClient httpClient;
 
 	@Inject
-	public OsioWitClient(final TokenIdentity auth, HttpClient client) {
-		this.authorization = requireNonNull(auth, ERROR_AUTHORIZATION_MUST_BE_SPECIFIED);
+	public OsioWitClient(final TokenIdentity token, HttpClient client) {
+		this.authorization = requireNonNull(token, ERROR_AUTHORIZATION_MUST_BE_SPECIFIED);
 		this.httpClient = requireNonNull(client, ERROR_HTTP_CLIENT_MUST_BE_SPECIFIED);
 	}
 
@@ -115,13 +112,31 @@ public class OsioWitClient {
 	 * @param stackId            the stackId
 	 * @param repositoryCloneUri the repository clone {@link URI}
 	 */
-	public void createCodeBase(final String spaceId, final String stackId, final URI repositoryCloneUri) {
+	public String createCodeBase(final String spaceId, final String stackId, final URI repositoryCloneUri) {
+
 		final String payload = String.format(
 				"{\"data\":{\"attributes\":{\"stackId\":\"%s\",\"type\":\"git\",\"url\":\"%s\"},\"type\":\"codebases\"}}",
 				stackId, repositoryCloneUri);
 		final Request request = newAuthorizedRequestBuilder(getWitUrl(), API_SPACES + spaceId + CODEBASES)
 				.post(create(parse(APPLICATION_JSON), payload)).build();
-		this.httpClient.executeAndConsume(request, r -> validateCodeBaseResponse(spaceId, repositoryCloneUri, r));
+
+		return this.httpClient.executeAndMap(request, response -> {
+			if (response.code() == 409) {
+				// Duplicate. This can be ignored for now as there is no connection in the
+				// 'beginning' of the wizard to verify what is in the codebase API
+				logger.log(Level.FINE,
+						   () -> "Duplicate codebase for spaceId " + spaceId + " and repository " + repositoryCloneUri);
+			} else {
+				try {
+					final String body = Objects.requireNonNull(response.body()).string();
+					return JsonUtils.readTree(body).get(DATA).get(ID).asText();
+				} catch (IOException e) {
+					throw new IllegalStateException(
+							"Error while reading response [" + response.toString() + "]: " + e.getMessage());
+				}
+			}
+			return "unknown-codebase-id";
+		});
 	}
 
 	/**
@@ -132,7 +147,7 @@ public class OsioWitClient {
 	 */
 	public Space createSpace(final String spaceName) {
 		final String payload = String.format("{\"data\":{\"attributes\":{\"name\":\"%s\"},\"type\":\"spaces\"}}",
-				spaceName);
+											 spaceName);
 		final Request request = newAuthorizedRequestBuilder(getWitUrl(), API_SPACES)
 				.post(create(parse(APPLICATION_JSON), payload)).build();
 		return this.httpClient.executeAndParseJson(request, OsioWitClient::readSpace)
@@ -151,9 +166,11 @@ public class OsioWitClient {
 			if (!response.isSuccessful()) {
 				String message = response.message();
 				try (ResponseBody body = response.body()) {
-					message = body.string();
-				} catch (IOException io) {
-					logger.log(Level.WARNING, io.getMessage(), io);
+					if (body != null) {
+						message = body.string();
+					}
+				} catch (IOException e) {
+					logger.log(Level.FINE, ERROR_RETRIEVING_DECODED_STRING_BODY + e.getMessage());
 				}
 				throw new HttpException(response.code(), message);
 			}
@@ -186,8 +203,8 @@ public class OsioWitClient {
 	private static List<Tenant.Namespace> readNamespaces(JsonNode tree) {
 		return StreamSupport.stream(tree.get(DATA).get(ATTRIBUTES).get(NAMESPACES).spliterator(), false)
 				.map(namespaceJson -> ImmutableNamespace.builder().name(namespaceJson.get(NAME).asText())
-						.type(namespaceJson.get(TYPE).asText()).clusterUrl(namespaceJson.get("cluster-url").asText())
-						.clusterConsoleUrl(namespaceJson.get("cluster-console-url").asText()).build())
+						.type(namespaceJson.get(TYPE).asText()).clusterUrl(namespaceJson.get(CLUSTER_URL).asText())
+						.clusterConsoleUrl(namespaceJson.get(CLUSTER_CONSOLE_URL).asText()).build())
 				.collect(Collectors.toList());
 	}
 
@@ -195,32 +212,5 @@ public class OsioWitClient {
 		final JsonNode data = tree.get(DATA);
 		final JsonNode attributes = data.get(ATTRIBUTES);
 		return ImmutableSpace.builder().id(data.get(ID).textValue()).name(attributes.get(NAME).textValue()).build();
-	}
-
-	private static void validateCodeBaseResponse(final String spaceId, final URI repositoryCloneUri,
-			final Response response) {
-		if (response.code() == 409) {
-			// Duplicate. This can be ignored for now as there is no connection in the
-			// 'beginning' of the wizard to
-			// verify what is in the codebase API
-			logger.log(Level.FINE,
-					() -> "Duplicate codebase for spaceId " + spaceId + " and repository " + repositoryCloneUri);
-		} else if (!response.isSuccessful()) {
-			assert response.body() != null;
-			String message = response.message();
-			try {
-				String body = response.body().string();
-				JsonNode errors = JsonUtils.readTree(body).get(ERRORS);
-				StringWriter sw = new StringWriter();
-				PrintWriter pw = new PrintWriter(sw, true);
-				for (JsonNode error : errors) {
-					pw.println(error.get(DETAIL).asText());
-				}
-				message = sw.toString();
-			} catch (IOException e) {
-				logger.log(Level.WARNING, "Error while reading error from WIT", e);
-			}
-			throw new HttpException(response.code(), message);
-		}
 	}
 }
