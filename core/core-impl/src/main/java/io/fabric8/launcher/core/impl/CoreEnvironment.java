84abdc6f2cbdd82a7fffff4d9b93a608b1be1df2
George Gastaldi <gegastaldi@gmail.com>
2018-10-29 15:29:25 -0300

    refactor(git): move LAUNCHER_BACKEND_GIT_REPOSITORY_DESCRIPTION to GitEnvironment

--- core/core-impl/src/main/java/io/fabric8/launcher/core/impl/CoreEnvironment.java
+++ core/core-impl/src/main/java/io/fabric8/launcher/core/impl/CoreEnvironment.java
@@ -6,7 +6,6 @@ import io.fabric8.launcher.base.EnvironmentEnum;
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public enum CoreEnvironment implements EnvironmentEnum {
-    LAUNCHER_BACKEND_GIT_REPOSITORY_DESCRIPTION,
     LAUNCHER_BACKEND_ENVIRONMENT,
     LAUNCHER_PREFETCH_BOOSTERS,
     LAUNCHER_BOOSTER_CATALOG_FILTER,
--- services/git-service-api/src/main/java/io/fabric8/launcher/service/git/GitEnvironment.java
+++ services/git-service-api/src/main/java/io/fabric8/launcher/service/git/GitEnvironment.java
@@ -6,6 +6,8 @@ import io.fabric8.launcher.base.EnvironmentEnum;
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public enum GitEnvironment implements EnvironmentEnum {
+    // TODO: Rename this to LAUNCHER_GIT_REPOSITORY_DESCRIPTION
+    LAUNCHER_BACKEND_GIT_REPOSITORY_DESCRIPTION,
     LAUNCHER_GIT_PROVIDER,
     LAUNCHER_GIT_COMMITTER_AUTHOR,
     LAUNCHER_GIT_COMMITTER_AUTHOR_EMAIL
