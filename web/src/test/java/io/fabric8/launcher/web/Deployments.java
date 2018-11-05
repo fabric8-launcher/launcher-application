c6731ffd6a84e844fdc993a19ca22729a4fae666
George Gastaldi <gegastaldi@gmail.com>
2018-11-05 21:04:07 +0000

    fix: add META-INF resources in WebArchive

--- web/src/test/java/io/fabric8/launcher/web/Deployments.java
+++ web/src/test/java/io/fabric8/launcher/web/Deployments.java
@@ -1,9 +1,14 @@
 package io.fabric8.launcher.web;
 
+import java.io.File;
+import java.util.Objects;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Util class that creates a shrinkwrap deployment used to create IT tests.
@@ -16,7 +21,8 @@ public class Deployments {
      * @return the war used for testing
      */
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class)
+
+        WebArchive archive = ShrinkWrap.create(WebArchive.class)
                 .addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"annotated\" version=\"1.1\"/>"),
                                      "beans.xml")
                 .addPackages(true, PackageMarker.class.getPackage())
@@ -24,5 +30,11 @@ public class Deployments {
                                         .loadPomFromFile("pom.xml")
                                         .importCompileAndRuntimeDependencies()
                                         .resolve().withTransitivity().asFile());
+
+        // Add META-INF files
+        for (File file: requireNonNull(new File("src/main/resources/META-INF").listFiles())) {
+            archive.addAsManifestResource(file);
+        }
+        return archive;
     }
 }
