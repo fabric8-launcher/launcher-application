5fa0680526eb7ddabee5b8086fa369aee8542540
tinakurian <tkurian@redhat.com>
2019-01-23 13:14:27 -0500

    adding golang customize test

---
+++ addons/osio-addon/src/test/java/io/fabric8/launcher/osio/steps/booster/GolangBoosterTest.java
@@ -0,0 +1,51 @@
+package io.fabric8.launcher.osio.steps.booster;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.google.common.io.Resources;
+
+import io.fabric8.launcher.booster.catalog.rhoar.Mission;
+import io.fabric8.launcher.booster.catalog.rhoar.RhoarBooster;
+import io.fabric8.launcher.booster.catalog.rhoar.Runtime;
+
+public class GolangBoosterTest {
+
+	@Test
+	public void testGolangBooster() {
+        // Mock a RhoarBooster
+        RhoarBooster booster = mock(RhoarBooster.class);
+        when(booster.getMission()).thenReturn(new Mission("http"));
+        when(booster.getRuntime()).thenReturn(new Runtime("golang"));
+
+        File file = new File(Resources.getResource("io/fabric8/launcher/osio/steps/booster").getPath());
+
+		GolangBooster goBooster = new GolangBooster(file.toPath(), setupBoosterTestData(), "testimportreplacement");
+		Map<File, String> response = goBooster.customize();
+
+		String content = response.get(new File(Resources.getResource("io/fabric8/launcher/osio/steps/booster/example.go").getPath()));
+		String line  = GolangBoosterUtility.getFileContents(new File(Resources.getResource("io/fabric8/launcher/osio/steps/booster/example.go").getPath())).get(4);
+
+		line = line.replace("golang-starters", "testimportreplacement");
+
+		assertTrue(content.contains(line));
+	}
+
+	private static Map<String, Object> setupBoosterTestData() {
+		Map<String, String> url = new HashMap<>();
+		Map<String, Object> git = new HashMap<>();
+		Map<String, Object> source = new HashMap<>();
+		url.put("url", "https://github.com/golang-starters/golang-rest-http");
+		git.put("git", url);
+		source.put("source", git);
+
+		return source;
+	}
+}
--- addons/osio-addon/src/test/resources/io/fabric8/launcher/osio/steps/booster/example.go
+++ addons/osio-addon/src/test/resources/io/fabric8/launcher/osio/steps/booster/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/golang-starters/golang-rest-http/app"
 )
 
 func main() {
