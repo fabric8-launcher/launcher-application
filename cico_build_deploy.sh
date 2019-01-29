4567d43103afae85f2ed447025d68b671b4261a7
George Gastaldi <gegastaldi@gmail.com>
2019-01-29 16:48:18 -0200

    chore: move to quay.io

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+set -x
+
 GENERATOR_DOCKER_HUB_USERNAME=openshiftioadmin
-REGISTRY_URI="push.registry.devshift.net"
+REGISTRY_URI="quay.io"
 REGISTRY_NS="fabric8"
 REGISTRY_IMAGE="launcher-backend"
 DOCKER_HUB_URL=${REGISTRY_NS}/${REGISTRY_IMAGE}
@@ -9,17 +11,17 @@ BUILDER_IMAGE="launcher-backend-builder"
 BUILDER_CONT="launcher-backend-builder-container"
 DEPLOY_IMAGE="launcher-backend-deploy"
 
-if [ "$TARGET" = "rhel" ]; then
-    REGISTRY_URL=${REGISTRY_URI}/osio-prod/${DOCKER_HUB_URL}
-    DOCKERFILE_DEPLOY="Dockerfile.deploy.rhel"
-else
-    REGISTRY_URL=${REGISTRY_URI}/${DOCKER_HUB_URL}
-    DOCKERFILE_DEPLOY="Dockerfile.deploy"
-fi
-
 TARGET_DIR="target"
 
-docker_login() {
+if [ "$TARGET" = "rhel" ]; then
+    REGISTRY_URL=${REGISTRY_URI}/openshiftio/rhel-${REGISTRY_NS}-${REGISTRY_IMAGE}
+    DOCKERFILE="Dockerfile.deploy.rhel"
+else
+    REGISTRY_URL=${REGISTRY_URI}/openshiftio/${REGISTRY_NS}-${REGISTRY_IMAGE}
+    DOCKERFILE="Dockerfile.deploy"
+fi
+
+function docker_login() {
     local USERNAME=$1
     local PASSWORD=$2
     local REGISTRY=$3
@@ -29,7 +31,7 @@ docker_login() {
     fi
 }
 
-tag_push() {
+function tag_push() {
     local TARGET_IMAGE=$1
 
     docker tag ${DEPLOY_IMAGE} ${TARGET_IMAGE}
@@ -39,44 +41,45 @@ tag_push() {
 # Exit on error
 set -e
 
-if [ -z "$CICO_LOCAL" ]; then
-    [ -f jenkins-env ] && cat jenkins-env | grep -e PASS -e GIT -e DEVSHIFT -e SONAR > inherit-env
+
+
+if [ -z $CICO_LOCAL ]; then
+    [ -f jenkins-env ] && cat jenkins-env | grep -e PASS -e USER -e GIT -e DEVSHIFT > inherit-env
     [ -f inherit-env ] && . inherit-env
 
     # We need to disable selinux for now, XXX
-    /usr/sbin/setenforce 0 || :
+    /usr/sbin/setenforce 0
 
     # Get all the deps in
     yum -y install docker make git
+
     service docker start
 fi
 
+#CLEAN
+docker ps | grep -q ${BUILDER_CONT} && docker stop ${BUILDER_CONT}
+docker ps -a | grep -q ${BUILDER_CONT} && docker rm ${BUILDER_CONT}
+
 #BUILD
-if [ ! -d "${TARGET_DIR}" ]; then
-    mkdir ${TARGET_DIR}
+docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
 
-    docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
+docker run --detach=true --name ${BUILDER_CONT} -t -v $(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE} /bin/tail -f /dev/null #FIXME
 
-    docker run --detach=true --name ${BUILDER_CONT} -t -v $(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE} /bin/tail -f /dev/null #FIXME
+#LOGIN
+docker_login "${QUAY_USERNAME}" "${QUAY_PASSWORD}" "${REGISTRY_URI}"
 
-    docker exec ${BUILDER_CONT} mvn -B clean install -DskipTests -Ddownload.plugin.skip.cache
-    docker exec -u root ${BUILDER_CONT} cp web/target/launcher-backend-thorntail.jar /${TARGET_DIR}
-fi
+#BUILD DEPLOY IMAGE
+docker build -t ${DEPLOY_IMAGE} -f "${DOCKERFILE}" .
 
 #PUSH
-if [ -z "$CICO_LOCAL" ]; then
-    docker_login ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
-
-    docker build -t ${DEPLOY_IMAGE} -f "${DOCKERFILE_DEPLOY}" .
-
+if [ -z $CICO_LOCAL ]; then
     TAG=$(echo $GIT_COMMIT | cut -c1-${DEVSHIFT_TAG_LEN})
 
     tag_push "${REGISTRY_URL}:${TAG}"
     tag_push "${REGISTRY_URL}:latest"
 
     if [[ "$TARGET" != "rhel" && -n "${GENERATOR_DOCKER_HUB_PASSWORD}" ]]; then
-        docker_login ${GENERATOR_DOCKER_HUB_USERNAME} ${GENERATOR_DOCKER_HUB_PASSWORD}
-
+        docker_login "${GENERATOR_DOCKER_HUB_USERNAME}" "${GENERATOR_DOCKER_HUB_PASSWORD}"
         tag_push "${DOCKER_HUB_URL}:${TAG}"
         tag_push "${DOCKER_HUB_URL}:latest"
     fi
