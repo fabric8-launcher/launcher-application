22a3467f276c726c8a881af195834c8c96479f54
Tina Kurian <tkurian@redhat.com>
2019-01-29 14:59:30 -0500

    Customize golang booster during create flow (#626)
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>

---
+++ addons/osio-addon/src/test/resources/io/fabric8/launcher/osio/enrichers/GitUserGoEnricherTest/example.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/golang-starters/golang-rest-http/app"
+)
+
+func main() {
+	// "github.com/golang-starters/golang-rest-http/app"
+}
