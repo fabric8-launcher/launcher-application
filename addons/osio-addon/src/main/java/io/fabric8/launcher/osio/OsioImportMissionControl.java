d512894894b87f7b6c932aa8e9b21d9e6dc505c8
George Gastaldi <gegastaldi@gmail.com>
2019-01-29 14:07:55 -0200

    chore: remove OpenShiftService.triggerBuild

--- addons/osio-addon/src/main/java/io/fabric8/launcher/osio/OsioImportMissionControl.java
+++ addons/osio-addon/src/main/java/io/fabric8/launcher/osio/OsioImportMissionControl.java
@@ -94,9 +94,6 @@ public class OsioImportMissionControl implements MissionControl<OsioImportProjec
         // Create jenkins config
         openShiftSteps.createJenkinsConfigMap(projectile, repository);
 
-        // Trigger the build in Openshift
-        openShiftSteps.triggerBuild(projectile);
-
         // Create Codebase in WIT
         final String cheStack = buildConfig.getMetadata().getAnnotations().get(Annotations.CHE_STACK);
         witSteps.createCodebase(projectile, cheStack, repository);
