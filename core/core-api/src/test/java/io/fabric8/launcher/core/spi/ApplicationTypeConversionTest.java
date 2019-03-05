00c49cef003c5d198c71c90d53f89750fb70f938
George Gastaldi <gegastaldi@gmail.com>
2019-03-05 00:50:07 -0300

    chore: convert core-api tests to JUnit 5

--- core/core-api/src/test/java/io/fabric8/launcher/core/spi/ApplicationTypeConversionTest.java
+++ core/core-api/src/test/java/io/fabric8/launcher/core/spi/ApplicationTypeConversionTest.java
@@ -1,6 +1,6 @@
 package io.fabric8.launcher.core.spi;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
