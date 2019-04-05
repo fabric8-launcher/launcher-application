148603f0f904480a51ea49c6be48ba34f17a66e3
George Gastaldi <gegastaldi@gmail.com>
2019-04-05 12:48:44 -0300

    chore: /health needs CORS (the Welcome app needs this)

--- web/src/main/java/io/fabric8/launcher/web/providers/CorsFilter.java
+++ web/src/main/java/io/fabric8/launcher/web/providers/CorsFilter.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
-@WebFilter(filterName = "CorsFilter", urlPatterns = "/api/*", asyncSupported = true)
+@WebFilter(filterName = "CorsFilter", urlPatterns = {"/api/*", "/health"}, asyncSupported = true)
 public class CorsFilter implements Filter {
 
     private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
