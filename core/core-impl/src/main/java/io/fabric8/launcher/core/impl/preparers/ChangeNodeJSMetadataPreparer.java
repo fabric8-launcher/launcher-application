e8559bb8d2ac2f08f7edce05717a3f546a2314e1
George Gastaldi <gegastaldi@gmail.com>
2019-04-17 18:21:23 +0000

    fix: be more lenient when changing Node.js properties that are not provided

--- core/core-impl/src/main/java/io/fabric8/launcher/core/impl/preparers/ChangeNodeJSMetadataPreparer.java
+++ core/core-impl/src/main/java/io/fabric8/launcher/core/impl/preparers/ChangeNodeJSMetadataPreparer.java
@@ -2,7 +2,6 @@ package io.fabric8.launcher.core.impl.preparers;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,6 +13,10 @@ import io.fabric8.launcher.core.api.ProjectileContext;
 import io.fabric8.launcher.core.api.projectiles.context.CoordinateCapable;
 import io.fabric8.launcher.core.api.projectiles.context.ProjectNameCapable;
 import io.fabric8.launcher.core.spi.ProjectilePreparer;
+
+import static io.fabric8.launcher.base.JsonUtils.readTree;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.readAllBytes;
 
 /**
  * Called by {@link io.fabric8.launcher.core.api.MissionControl#prepare(ProjectileContext)}
@@ -31,11 +34,18 @@ public class ChangeNodeJSMetadataPreparer implements ProjectilePreparer {
 
         CoordinateCapable coordinateCapable = (CoordinateCapable) context;
         Path packageJsonPath = projectPath.resolve("package.json");
-        if (Files.exists(packageJsonPath)) {
+        if (exists(packageJsonPath)) {
             try {
-                final ObjectNode packageJson = (ObjectNode) JsonUtils.readTree(new String(Files.readAllBytes(packageJsonPath)));
-                packageJson.put("name", ((ProjectNameCapable)context).getProjectName());
-                packageJson.put("version", coordinateCapable.getProjectVersion());
+                final String projectName = ((ProjectNameCapable) context).getProjectName();
+                final String projectVersion = coordinateCapable.getProjectVersion();
+
+                final ObjectNode packageJson = (ObjectNode) readTree(new String(readAllBytes(packageJsonPath)));
+                if (projectName != null) {
+                    packageJson.put("name", projectName);
+                }
+                if (projectVersion != null) {
+                    packageJson.put("version", projectVersion);
+                }
                 JsonUtils.writeTree(packageJson, packageJsonPath.toFile());
             } catch (IOException e) {
                 throw new UncheckedIOException("Error while reading " + packageJsonPath, e);
