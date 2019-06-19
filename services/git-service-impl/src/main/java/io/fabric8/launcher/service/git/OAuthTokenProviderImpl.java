f6e63583504ea709083b3b431d39bd28f5dec8eb
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-06-19 11:25:45 +0200

    updated gastaldi comments

--- services/git-service-impl/src/main/java/io/fabric8/launcher/service/git/OAuthTokenProviderImpl.java
+++ services/git-service-impl/src/main/java/io/fabric8/launcher/service/git/OAuthTokenProviderImpl.java
@@ -1,20 +1,19 @@
 package io.fabric8.launcher.service.git;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.fabric8.launcher.base.JsonUtils;
+import io.fabric8.launcher.base.http.HttpClient;
 import io.fabric8.launcher.service.git.api.GitServiceConfig;
 import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.enterprise.context.ApplicationScoped;
-import javax.json.Json;
-import javax.json.JsonObject;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -22,28 +21,36 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static io.fabric8.launcher.base.http.Requests.APPLICATION_JSON;
 import static okhttp3.RequestBody.create;
 
-@ApplicationScoped
+@Singleton
 public class OAuthTokenProviderImpl implements OAuthTokenProvider {
 
-    private final OkHttpClient client;
+    private final HttpClient client;
 
     private final KeyPair keyPair;
     private final Cipher cipher;
 
-    OAuthTokenProviderImpl() throws NoSuchAlgorithmException, NoSuchPaddingException {
+    @Inject
+    OAuthTokenProviderImpl(HttpClient client) throws NoSuchAlgorithmException, NoSuchPaddingException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         cipher = Cipher.getInstance("RSA");
         keyPair = keyPairGenerator.generateKeyPair();
-        client = new OkHttpClient.Builder().build();
+        this.client = client;
     }
 
     @Override
     public String getToken(String code, GitServiceConfig config) {
-        String token = fetchAccessToken(code, config);
+        String token;
+        try {
+            token = fetchAccessToken(code, config).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException(e);
+        }
         return encodeToken(token);
     }
 
@@ -56,24 +63,22 @@ public class OAuthTokenProviderImpl implements OAuthTokenProvider {
         }
     }
 
-    private String fetchAccessToken(String code, GitServiceConfig config) {
-        JsonObject object = Json.createObjectBuilder()
-                .add("client_id", config.getServerProperties().get("clientId"))
-                .add("client_secret", config.getServerProperties().get("clientSecret"))
-                .add("code", code).build();
-        StringWriter writer = new StringWriter();
-        Json.createWriter(writer).write(object);
+    private CompletableFuture<String> fetchAccessToken(String code, GitServiceConfig config) {
+        ObjectNode object = JsonUtils.createObjectNode()
+                .put("client_id", config.getServerProperties().get("clientId"))
+                .put("client_secret", config.getServerProperties().get("clientSecret"))
+                .put("code", code);
         Request request = new Request.Builder()
                 .url(config.getServerProperties().get("oauthUrl"))
-                .post(create(APPLICATION_JSON, writer.toString())).build();
+                .post(create(APPLICATION_JSON, object.toString())).build();
 
-        Response response;
-        try {
-            response = client.newCall(request).execute();
-            return parseResult(Objects.requireNonNull(response.body()).string());
-        } catch (IOException e) {
-            throw new RuntimeException("Could not fetch access token", e);
-        }
+        return client.executeAndMapAsync(request, response -> {
+            try {
+                return parseResult(Objects.requireNonNull(response.body()).string());
+            } catch (IOException e) {
+                throw new RuntimeException("Could not fetch access token", e);
+            }
+        });
     }
 
     private String parseResult(String response) {
