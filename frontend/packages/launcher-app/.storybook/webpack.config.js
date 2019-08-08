d60c72cb1a00b91992a718cc95c2aa934eb0f2c2
ia3andy <adamevin@redhat.com>
2019-08-08 12:57:45 +0200

    fix: make env-cmd work again

--- frontend/packages/launcher-app/.storybook/webpack.config.js
+++ frontend/packages/launcher-app/.storybook/webpack.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-module.exports = (baseConfig, env, config) => {
+module.exports = ({config}) => {
     config.module.rules.push({
         test: /\.(ts|tsx)$/,
         use: [{
             loader: require.resolve('awesome-typescript-loader'),
-        }, {
-            loader: require.resolve('react-docgen-typescript-loader'),
         }]
     });
     config.module.rules.push({
