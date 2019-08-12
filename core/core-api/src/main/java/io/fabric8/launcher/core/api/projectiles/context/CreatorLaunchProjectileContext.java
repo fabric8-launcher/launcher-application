06a1100582f844dab9e6dbfd873fe60f49942999
George Gastaldi <gegastaldi@gmail.com>
2019-08-12 20:52:02 +0000

    refactor: refactored IdentityProvider

--- core/core-api/src/main/java/io/fabric8/launcher/core/api/projectiles/context/CreatorLaunchProjectileContext.java
+++ core/core-api/src/main/java/io/fabric8/launcher/core/api/projectiles/context/CreatorLaunchProjectileContext.java
@@ -1,17 +1,17 @@
 package io.fabric8.launcher.core.api.projectiles.context;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.immutables.value.Value;
 
-import javax.validation.constraints.NotNull;
-
 @Value.Immutable
 @JsonSerialize(as = ImmutableCreatorLaunchProjectileContext.class)
 @JsonDeserialize(as = ImmutableCreatorLaunchProjectileContext.class)
-@JsonIgnoreProperties("clusterId")
+@JsonIgnoreProperties({"clusterId", "clusterType"})
 public interface CreatorLaunchProjectileContext extends CreatorLaunchingProjectileContext {
     @NotNull ObjectNode getProject();
 }
