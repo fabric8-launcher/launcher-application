f98ddd654170066673edd6d0f1d9f871b7b1a7d6
ia3andy <adamevin@redhat.com>
2019-08-13 11:44:00 +0200

    fix: update deps and fix storybook

--- frontend/packages/launcher-app/src/flows/__tests__/flows.stories.tsx
+++ frontend/packages/launcher-app/src/flows/__tests__/flows.stories.tsx
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { CreateNewAppFlow } from '../create-new-app-flow';
 import { DeployExampleAppFlow } from '../deploy-example-app-flow';
 import { ImportExistingFlow } from '../import-existing-flow';
-import { LauncherDepsProvider } from '../..';
+import { LauncherDepsProvider } from '../../contexts/launcher-client-provider';
 
 storiesOf('Flows', module)
   .addDecorator((storyFn) => (
--- frontend/packages/launcher-app/src/next-steps/__tests__/misc.stories.tsx
+++ frontend/packages/launcher-app/src/next-steps/__tests__/misc.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import { storiesOf } from '@storybook/react';
-import { LauncherDepsProvider } from '../..';
 import { LaunchNextSteps } from '../launch-next-steps';
 import { action } from '@storybook/addon-actions';
 import { DownloadNextSteps } from '../download-next-steps';
+import { LauncherDepsProvider } from '../../contexts/launcher-client-provider';
 
 storiesOf('Misc', module)
   .addDecorator((storyFn) => (
