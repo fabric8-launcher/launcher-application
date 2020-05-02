7c13032285e411a90d9bbec5f6c91327f445a992
Tako Schotanus <tako@codejive.org>
2020-05-02 20:31:26 +0200

    chore(helm): a couple of minor fixes and additions

--- charts/update-release-values.sh
+++ charts/update-release-values.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-echo application.image.tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-application/commits/HEAD | jq -r '.sha' | cut -c1-7) > release-values.yaml
-echo welcome.image.tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-creator-welcome-app/commits/HEAD | jq -r '.sha' | cut -c1-7) >> release-values.yaml
-
+echo -e "application:\n  image:\n    tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-application/commits/HEAD | jq -r '.sha' | cut -c1-7)" > release-values.yaml
+echo -e "welcome:\n  image:\n    tag: $(curl -s https://api.github.com/repos/fabric8-launcher/launcher-creator-welcome-app/commits/HEAD | jq -r '.sha' | cut -c1-7)" >> release-values.yaml
