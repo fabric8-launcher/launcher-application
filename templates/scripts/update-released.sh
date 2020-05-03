32f6c89712f4725a88612e1192249d42e4e55618
Tako Schotanus <tako@codejive.org>
2020-05-03 04:11:14 +0200

    refactor: Removed templates, use Helm to install the Launcher

--- templates/scripts/update-released.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo BACKEND_IMAGE_TAG=$(curl -s https://api.github.com/repos/fabric8-launcher/launcher-application/commits/HEAD | jq -r '.sha' | cut -c1-7) >> released.properties
-echo WELCOME_IMAGE_TAG=$(curl -s https://api.github.com/repos/fabric8-launcher/launcher-creator-welcome-app/commits/HEAD | jq -r '.sha' | cut -c1-7) >> released.properties
